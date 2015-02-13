297ec02ee2e080006d6d58e253a0d0a05467748b
Gerry <weirich@elexis.ch>
2015-02-13 12:32:45 +0100

    logjn ok

--- webelexis-agenda/src/main/java/ch/webelexis/agenda/AgendaHandler.java
+++ webelexis-agenda/src/main/java/ch/webelexis/agenda/AgendaHandler.java
@@ -18,10 +18,13 @@ import org.vertx.java.core.json.JsonObject;
  */
 public class AgendaHandler implements Handler<Message<JsonObject>> {
 	EventBus eb;
-	
-	AgendaHandler(EventBus eb){
-		this.eb=eb;
+	static final String ELEXISDATE = "20[0-9]{6,6}";
+	static final String NAME = "[0-9a-zA-Z ]+";
+
+	AgendaHandler(EventBus eb) {
+		this.eb = eb;
 	}
+
 	/*
 	 * this is, what mod_mysql expects { "action" : "prepared", "statement" :
 	 * "SELECT * FROM some_test WHERE name=? AND money > ?", "values" :
@@ -31,10 +34,71 @@ public class AgendaHandler implements Handler<Message<JsonObject>> {
 	 * "end": "yyyymmdd", "resource": resource, "token": auth-token }
 	 */
 	@Override
-	public void handle(final Message<JsonObject> event) {
-		JsonObject request = event.body();
-		// TODO authorize
-		Server.log.info("received request");
+	public void handle(final Message<JsonObject> externalRequest) {
+		final JsonObject request = externalRequest.body();
+		String token = request.getString("token");
+		if (token == null) {
+			handlePublic(externalRequest, request);
+		} else {
+			eb.send("ch.webelexis.auth.authorise",
+					new JsonObject().putString("sessionID", token),
+					new Handler<Message<JsonObject>>() {
+
+						@Override
+						public void handle(Message<JsonObject> localAnswer) {
+							if (localAnswer.body().getString("status")
+									.equalsIgnoreCase("ok")) {
+								handleAuthorized(externalRequest, request);
+
+							} else {
+								handlePublic(externalRequest, request);
+							}
+
+						}
+					});
+
+			Server.log.info("received request");
+		}
+	}
+
+	/**
+	 * This is what a unauthorized client gets
+	 * 
+	 * @param event
+	 * @param request
+	 */
+	private void handlePublic(final Message<JsonObject> event,
+			JsonObject request) {
+		JsonObject bridge = new JsonObject()
+				.putString("action", "prepared")
+				.putString(
+						"statement",
+						"SELECT Tag,Beginn,Dauer from AGNTERMINE where Tag>=? and Tag <=? and Bereich=? and deleted='0'")
+				.putArray(
+						"values",
+						new JsonArray(new String[] {
+								getCleaned(request, "begin", ELEXISDATE),
+								getCleaned(request, "end", ELEXISDATE),
+								getCleaned(request, "resource", NAME) }));
+		eb.send("ch.webelexis.sql", bridge, new Handler<Message<JsonObject>>() {
+
+			@Override
+			public void handle(Message<JsonObject> returnvalue) {
+				JsonObject res = returnvalue.body();
+				// TODO remove unneeded fields
+				event.reply(res);
+			}
+		});
+	}
+
+	/**
+	 * This is, what an authorized user gets
+	 * 
+	 * @param event
+	 * @param request
+	 */
+	private void handleAuthorized(final Message<JsonObject> event,
+			JsonObject request) {
 		JsonObject bridge = new JsonObject()
 				.putString("action", "prepared")
 				.putString(
@@ -43,18 +107,26 @@ public class AgendaHandler implements Handler<Message<JsonObject>> {
 				.putArray(
 						"values",
 						new JsonArray(new String[] {
-								request.getString("begin"),
-								request.getString("end"),
-								request.getString("resource") }));
-				eb.send("ch.webelexis.sql",bridge, new Handler<Message<JsonObject>>() {
+								getCleaned(request, "begin", ELEXISDATE),
+								getCleaned(request, "end", ELEXISDATE),
+								getCleaned(request, "resource", NAME) }));
+		eb.send("ch.webelexis.sql", bridge, new Handler<Message<JsonObject>>() {
 
-					@Override
-					public void handle(Message<JsonObject> returnvalue) {
-						JsonObject res=returnvalue.body();
-						// TODO remove unneeded fields
-						event.reply(res);
-					}
-				});
+			@Override
+			public void handle(Message<JsonObject> returnvalue) {
+				JsonObject res = returnvalue.body();
+				// TODO remove unneeded fields
+				event.reply(res);
+			}
+		});
 	}
 
+	private String getCleaned(JsonObject jo, String field, String pattern) {
+		String raw = jo.getString(field);
+		if ((raw != null) && raw.matches(pattern)) {
+			return raw;
+		} else {
+			return "";
+		}
+	}
 }
--- webelexis-agenda/web/view-model.js
+++ webelexis-agenda/web/view-model.js
@@ -2,44 +2,49 @@
 //var console= require ('vertx/console');
 var eb;
 
-function appointment(from, to, reason){
-    var self=this;
-    self.from= ko.observable(from);
-    self.until=ko.observable(to);
-    self.reason=reason;
+function appointment(from, to, reason) {
+	var self = this;
+	self.from = ko.observable(from);
+	self.until = ko.observable(to);
+	self.reason = reason;
 }
 function AgendaViewModel() {
-    var self=this;
-    
-    self.appointments = ko.observableArray([
-        new appointment("08:00","08:30","quatsch")
-    ]);
-    
-    self.load = function(){
-      eb.send('ch.webelexis.agenda.appointments',{begin: "1", end: "2", resource: "3"}, function(jsonResult){
-        //self.appointments.clear
-          var result=/* JSON.parse */(jsonResult)
-          console.log("result: "+JSON.stringify(result));
-        if(result.status != "ok"){
-            self.appointments.push(new appointment("---",result.status,"error"));
-        }else{
-            result.values.foreach(function(value){
-                self.appointments.push(new appointment("a","b","c"))
-            });
-        }
-      });
-    }
+	var self = this;
+
+	self.appointments = ko.observableArray([ new appointment("08:00", "08:30",
+			"quatsch") ]);
+
+	self.load = function() {
+		eb.send('ch.webelexis.agenda.appointments', {
+			begin : "20150212",
+			end : "20150213",
+			resource : "gerry"
+		}, function(jsonResult) {
+			// self.appointments.clear
+			var result = /* JSON.parse */(jsonResult)
+			console.log("result: " + JSON.stringify(result));
+			if (result.status != "ok") {
+				self.appointments.push(new appointment("---", result.status,
+						"error"));
+			} else {
+				var appnts=result.results;
+				appnts.forEach(function(value) {
+					self.appointments.push(new appointment(value[0], value[1], value[2]))
+				});
+			}
+		});
+	}
 }
 
 function initialize() {
-    eb = new vertx.EventBus('http://localhost:8080/eventbus');
-    eb.onopen = function() {
-        console.log("eventbus ok");   
-    }
-    eb.onclose = function() {
-        console.log("eventbus closed")
-    }
+	eb = new vertx.EventBus('http://localhost:8080/eventbus');
+	eb.onopen = function() {
+		console.log("eventbus ok");
+	}
+	eb.onclose = function() {
+		console.log("eventbus closed")
+	}
 }
-              
+
 initialize();
 ko.applyBindings(new AgendaViewModel());
