4ffd7a115bbf727ad77fc2b5cec42085396a9890
gerry <weirich@elexis.ch>
2015-02-15 17:23:57 +0100

    css fiddles

--- webelexis-agenda/src/main/java/ch/webelexis/agenda/SqlMock.java
+++ webelexis-agenda/src/main/java/ch/webelexis/agenda/SqlMock.java
@@ -8,14 +8,24 @@ import org.vertx.java.core.json.JsonObject;
 public class SqlMock implements Handler<Message<JsonObject>> {
 	//String retval="{ 'status': 'ok', [{'10:00','10:30','uops'},{'10:30','11:00','hach'}]}";
 	JsonObject retval=new JsonObject().putString("status", "ok");
-	JsonArray ja=new JsonArray();
-	JsonArray jb=new JsonArray(new String[]{
-			"10:00","10:30","test"
-	});
 	
 	@Override
 	public void handle(Message<JsonObject> event) {
-		retval.putArray("results", jb);
+		JsonArray ja=new JsonArray();
+		JsonArray jb=new JsonArray(new String[]{
+				
+				"20150213","600","30","123456afjfdhssdg","Müller","Fritz","Normal","geplant","Schmerz da und dort"
+		});
+		JsonArray jc=new JsonArray(new String[]{
+				"20150213","660","45","12ff56afefdhaadc","Meier","Anna","Normal","geplant","Schnupfen"
+				
+		});
+		
+
+		ja.add(jb);
+		ja.add(jc);
+		retval.putArray("results", ja);
+		
 		event.reply(retval);
 	}
 
--- webelexis-agenda/web/view-model.js
+++ webelexis-agenda/web/view-model.js
@@ -1,111 +1,152 @@
 //var vertx = require('vertx');
 //var console= require ('vertx/console');
 var eb;
-var sessionid=""
-var convert=new ElexisTime()
-	
-function appointment(from, to, reason) {
-	var self = this;
-	self.from = ko.observable(from);
-	self.until = ko.observable(to);
-	self.reason = reason;
+var sessionid = ""
+var convert = new ElexisTime()
+
+function appointment(row) {
+    var self = this;
+    self.date = convert.makeDate(row[0])
+    var test = parseInt(row[1]);
+    self.begin = convert.makeTime(parseInt(row[1]));
+    self.end = convert.makeTime(parseInt(row[1]) + parseInt(row[2]));
+    self.time = self.begin + "-" + self.end
+    self.patid = row[3];
+    self.patName = row[4];
+    self.firstName = row[5];
+    self.patient = self.patName + " " + self.firstName;
+    self.type = row[6];
+    self.state = row[7];
+    self.reason = row[8];
 }
+
 function AgendaViewModel() {
-	var self = this;
+    var self = this;
 
-	self.appointments = ko.observableArray([ new appointment("08:00", "08:30",
-			"quatsch") ]);
+    self.appointments = ko.observableArray([]);
 
-	self.load = function() {
-		eb.send('ch.webelexis.agenda.appointments', {
-			begin : "20150212",
-			end : "20150213",
-			resource : "gerry",
-			token: sessionid
-		}, function(jsonResult) {
-			// self.appointments.removeAll
-			var result = /* JSON.parse */(jsonResult)
-			console.log("result: " + JSON.stringify(result));
-			if (result.status != "ok") {
-				self.appointments.push(new appointment("---", result.status,
-						"error"));
-			} else {
-				var appnts=result.results;
-				appnts.forEach(function(value) {
-					self.appointments.push(new appointment(value[0], value[1], value[2]))
-				});
-			}
-		});
-	}
+    self.load = function () {
+        var selected = $("#datumfeld").val();
+        console.log(selected)
+        eb.send('ch.webelexis.agenda.appointments', {
+            begin: $("#datumfeld").val(),
+            end: $("datumfeld").val(),
+            resource: "gerry",
+            token: sessionid
+        }, function (jsonResult) {
+            self.appointments.removeAll()
+            var result = /* JSON.parse */ (jsonResult)
+            console.log("result: " + JSON.stringify(result));
+            if (result.status != "ok") {
+                self.appointments.push(new appointment("---", result.status,
+                    "error"));
+            } else {
+                var appnts = result.results;
+                appnts.forEach(function (value) {
+                    self.appointments.push(new appointment(value))
+                });
+            }
+        });
+
+
+    }
+
+    self.isEven = function(n) {
+        return isNumber(n) && (n % 2 == 0);
+    }
+    
+    self.clear = function () {
+        self.appointments.removeAll()
+    }
 }
 
-function ElexisTime(){
-	var self=this
-	
-	self.makeDate=function(datestring){
-		var year=datestring.substring(0,4)
-		var month=datestring.substring(4,6)
-		var day=datestring.substring(6,8)
-		return new Date(year,month,day)
-	}
-	
-	self.makeString=function(date){
-		var year=date.getFullYear()
-		var month=date.getMonth().toString();
-		if(month.length<2){
-			month='0'+month
-		}
-		var day=date.getDate().toString();
-		if(day.length<2){
-			day='0'+day
-		}
-		return year.toString()+month+day
-	}
-	
-	self.makeTime=function(minutes){
-		var hours=parseInt(minutes/60)
-		var mins=minutes-(hours*60)
-		return hours.toString()+":"+mins
-	}
+function ElexisTime() {
+    var self = this
+
+    self.makeDate = function (datestring) {
+        var year = datestring.substring(0, 4)
+        var month = datestring.substring(4, 6)
+        var day = datestring.substring(6, 8)
+        return new Date(year, month, day)
+    }
+
+    self.makeString = function (date) {
+        var year = date.getFullYear()
+        var month = date.getMonth().toString();
+        if (month.length < 2) {
+            month = '0' + month
+        }
+        var day = date.getDate().toString();
+        if (day.length < 2) {
+            day = '0' + day
+        }
+        return year.toString() + month + day
+    }
+
+    self.makeTime = function (minutes) {
+        var hours = parseInt(minutes / 60)
+        var mins = (minutes - (hours * 60)).toString()
+        hours = hours.toString()
+        if (hours.length < 2) {
+            hours = "0" + hours
+        }
+        if (mins.length < 2) {
+            mins = "0" + mins
+        }
+
+        return hours + ":" + mins
+    }
 }
 
-function dologin(name,pwd){
-	eb.send('ch.webelexis.auth.login', {
-		username: name,
-		password: pwd
-	}, function(result){
-		if(result.status== "ok"){
-			sessionid=result.sessionID;
-			$("#username").val("")
-			$("#pwd").val("")
-			$("#userLogin").hide()
-			$("#userLogout").show()
-			$("#loggedInUser").text(name)
-		}
-	});
+function dologin(name, pwd) {
+    eb.send('ch.webelexis.auth.login', {
+        username: name,
+        password: pwd
+    }, function (result) {
+        if (result.status == "ok") {
+            sessionid = result.sessionID;
+            $("#badlogin").hide()
+            $("#username").val("")
+            $("#pwd").val("")
+            $("#userLogin").hide()
+            $("#userLogout").show()
+            $("#loggedInUser").text(name)
+        } else {
+            $("#badlogin").show()
+        }
+    });
 }
 
-function dologout(){
-	eb.send('ch.webelexis.auth.logout', {
-		"sessionID": sessionid
-	})
-	sessionid=""
-	$("#userLogin").show()
-	$("#userLogout").hide()
+function dologout() {
+    eb.send('ch.webelexis.auth.logout', {
+        "sessionID": sessionid
+    })
+    sessionid = ""
+    $("#userLogin").show()
+    $("#userLogout").hide()
 
 }
 
 function initialize() {
-	eb = new vertx.EventBus('http://localhost:8080/eventbus');
-	eb.onopen = function() {
-		console.log("eventbus ok");
-	}
-	eb.onclose = function() {
-		console.log("eventbus closed")
-	}
+    eb = new vertx.EventBus('http://localhost:8080/eventbus');
+    eb.onopen = function () {
+        console.log("eventbus ok");
+    }
+    eb.onclose = function () {
+        console.log("eventbus closed")
+    }
 }
 
 
-$("#userLogout").hide()
+$("#userLogout").hide();
+$("#badlogin").hide();
 initialize();
+$('.datepicker').datepicker({
+    'format': "dd.mm.yyyy",
+    'autoclose': true,
+    'language': "de",
+    todayBtn: "linked",
+    todayHighlight: true
+})
+$('.datepicker').datepicker('setDate', new Date())
 ko.applyBindings(new AgendaViewModel());
