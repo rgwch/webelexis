ab0dad7025f947752058544af47dcbb846547ac7
gerry <weirich@elexis.ch>
2015-03-15 09:24:37 +0100

    login and logout

--- webelexis-base/src/main/resources/web/components/menubar/ch-webelexis-menubar.js
+++ webelexis-base/src/main/resources/web/components/menubar/ch-webelexis-menubar.js
@@ -1,11 +1,54 @@
-define(['app/config','knockout','text!ch-webelexis-menubar.html', 'domReady!'],function(cfg,ko,html){
-    function MenubarModel(params){
-        var self=this
-        self.menuItems=ko.observableArray(cfg[params.menu])
-        //console.log(self.menu()[0].baseUrl+","+self.menu()[0].title)
-        //self.menuItems=ko.observableArray([{baseUrl: '#agenda', title: 'Agenda'},{baseUrl: 'haha', title: 'HiHi'}])
+define(['app/config', 'knockout', 'text!ch-webelexis-menubar.html', 'app/eb', 'domReady!'], function (cfg, ko, html, bus) {
+    function MenubarModel(params) {
+        var self = this
+        self.menuItems = ko.observableArray(cfg[params.menu])
+            //console.log(self.menu()[0].baseUrl+","+self.menu()[0].title)
+            //self.menuItems=ko.observableArray([{baseUrl: '#agenda', title: 'Agenda'},{baseUrl: 'haha', title: 'HiHi'}])
+        self.unam = ko.observable("hans")
+        self.upwd = ko.observable()
+        self.doLogin = function () {
+            bus.send("ch.webelexis.auth.login", {
+                username: self.unam(),
+                password: self.upwd()
+            }, function (result) {
+                if (result.status === "ok") {
+                    cfg.sessionID = result.sessionID
+                    if (result.roles === undefined || result.roles.length > 1) {
+                        cfg.roles = []
+                    } else {
+                        cfg.roles = result.roles
+                    }
+                    self.menuItems.clear()
+                    for (var i = 0; i < cfg[params.menu].length; i++) {
+                        var item = cfg[params.menu][i]
+                        var newMenu = []
+                        item.roles.forEach(function (element) {
+                            if (cfg.roles.indexOf(element) !== -1) {
+                                newMenu.push(item)
+                            }
+                        })
+                    }
+                    $("#loginform").addClass("hidden")
+                    $("#logged-in-text").removeClass("hidden")
+                } else {
+                    $("#badlogin-text").removeClass("hidden")
+                }
+            })
+        }
+        self.doLogout = function () {
+            bus.send("ch.webelexis.auth.logout", {
+                sessionID: cfg.sessionID
+            }, function (result) {
+                if(result.status==="ok"){
+                    cfg.sessionID=""
+                    cfg.roles=[]
+                    $("#loginform").removeClass("hidden")
+                    $("#logged-in-text").addClass("hidden")
+                }
+            })
+        }
     }
-    return{
+    return {
         viewModel: MenubarModel,
         template: html
     }
