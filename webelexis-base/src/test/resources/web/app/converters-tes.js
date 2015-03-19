b1d876ef894b5b8697e40461d437034feb4fd4e4
Gerry Weirich <weirich@elexis.ch>
2015-03-19 19:56:39 +0100

    test cases

---
+++ webelexis-base/src/test/resources/web/app/converters-tes.js
@@ -0,0 +1,5 @@
+define(['components/agenda/ch-webelexis-agenda'], function (agenda) {
+    var res = new agenda.viewModel()
+    console.log(res.readDate())
+
+});
---
+++ webelexis-base/src/test/resources/web/app/test-main.js
@@ -0,0 +1,46 @@
+var allTestFiles = [];
+var TEST_REGEXP = /(spec|test)\.js$/i;
+
+var pathToModule = function (path) {
+    return path.replace(/^\/base\//, '').replace(/\.js$/, '');
+};
+
+Object.keys(window.__karma__.files).forEach(function (file) {
+    if (TEST_REGEXP.test(file)) {
+        // Normalize paths to RequireJS module names.
+        allTestFiles.push(pathToModule(file));
+    }
+});
+
+require.config({
+    // Karma serves files under /base, which is the basePath from your config file
+    baseUrl: '/base',
+    paths: {
+        "bootstrap": "dist/web/lib/bootstrap.min",
+        "jquery": "dist/web/lib/jquery.min",
+        "jquery-ui": "dist/web/lib/jquery-ui",
+        "knockout": "dist/web/lib/knockout",
+        "sockjs": "dist/web/lib/sockjs.min",
+        "text": "dist/web/lib/text",
+        "vertxbus": "dist/web/lib/vertxbus",
+        "domReady": "dist/web/lib/domReady",
+        "knockout-jqueryui": "dist/web/lib/knockout-jqueryui",
+        "chai": "node_modules/chai/lib/chai",
+        "app": "dist/web/app",
+        "components": "dist/web/components"
+    },
+    shim: {
+        "bootstrap": {
+            deps: ["jquery"]
+        },
+        "knockout": {
+            deps: ["jquery"]
+        },
+    },
+
+    // dynamically load all test files
+    deps: allTestFiles,
+
+    // we have to kickoff jasmine, as it is asynchronous
+    callback: window.__karma__.start
+});
