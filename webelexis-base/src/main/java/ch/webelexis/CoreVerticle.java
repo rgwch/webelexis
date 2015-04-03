b243a7bbe5221fa818c4efc453e40da403d89e0a
Gerry Weirich <weirich@elexis.ch>
2015-04-03 18:35:07 +0200

    move authentication and session management to external module (ch.rgw~vertx-mod-sessionmgr~0.1.0)

--- webelexis-base/src/main/java/ch/webelexis/CoreVerticle.java
+++ webelexis-base/src/main/java/ch/webelexis/CoreVerticle.java
@@ -5,11 +5,9 @@
 package ch.webelexis;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import org.vertx.java.busmods.BusModBase;
 import org.vertx.java.core.AsyncResult;
@@ -17,7 +15,6 @@ import org.vertx.java.core.AsyncResultHandler;
 import org.vertx.java.core.Future;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.http.HttpServer;
-import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.core.sockjs.SockJSServer;
@@ -45,22 +42,21 @@ public class CoreVerticle extends BusModBase {
 	long waitingTime;
 
 	/**
-	 * Enter all modules and verticles to deploy here. Note: Webelexis will
-	 * deploy them asynchronously, so there is no guaranteed order for them to
-	 * be ready. Modules don't need to exist locally. The system will fetch them
-	 * from the vert.x module repository as needed.
+	 * Enter all modules and verticles to deploy here. Note: Webelexis will deploy
+	 * them asynchronously, so there is no guaranteed order for them to be ready.
+	 * Modules don't need to exist locally. The system will fetch them from the
+	 * vert.x module repository as needed.
 	 */
-	V[] modules = new V[] {
-			new V("sql", "io.vertx~mod-mysql-postgresql_2.10~0.3.1"),
-			new V("mongo", "io.vertx~mod-mongo-persistor~2.1.0") };
-	V[] verticles = new V[] { new V("auth", "ch.rgw.vertx.AuthManager"),
-			new V("agenda", "ch.webelexis.agenda.Server") };
+	V[] modules = new V[] { new V("sql", "io.vertx~mod-mysql-postgresql_2.10~0.3.1"),
+			new V("mongo", "io.vertx~mod-mongo-persistor~2.1.0"), new V("auth", "ch.rgw~vertx-mod-sessionmgr~0.1.0") };
+
+	V[] verticles = new V[] { new V("agenda", "ch.webelexis.agenda.Server") };
 
 	public CoreVerticle() throws IOException {
 		File file = new File("config_defaults.json"); // production mode
 		if (!file.exists()) {
 			file = new File("src/main/resources/config_sample.json"); // IDE
-																		// mode
+			// mode
 		}
 		char[] buffer = new char[(int) file.length()];
 		FileReader fr = new FileReader(file);
@@ -77,13 +73,11 @@ public class CoreVerticle extends BusModBase {
 		log.debug("CoreVerticle got config: " + cfg.encodePrettily());
 		for (V m : modules) {
 			pending.add(m.title);
-			container.deployModule(m.fullname, cfg.getObject(m.title),
-					new DeploymentHandler(m.title));
+			container.deployModule(m.fullname, cfg.getObject(m.title), new DeploymentHandler(m.title));
 		}
 		for (V v : verticles) {
 			pending.add(v.title);
-			container.deployVerticle(v.fullname, cfg.getObject(v.title),
-					new DeploymentHandler(v.title));
+			container.deployVerticle(v.fullname, cfg.getObject(v.title), new DeploymentHandler(v.title));
 		}
 
 		waitingTime = System.currentTimeMillis();
@@ -97,8 +91,7 @@ public class CoreVerticle extends BusModBase {
 				} else {
 					if ((System.currentTimeMillis() - waitingTime) > TIMEOUT) {
 						vertx.cancelTimer(timerID);
-						startedResult.setFailure(new Exception(
-								"Timeout waiting for launching modules"));
+						startedResult.setFailure(new Exception("Timeout waiting for launching modules"));
 					}
 				}
 				if (reason != null) {
@@ -111,8 +104,8 @@ public class CoreVerticle extends BusModBase {
 		HttpServer http = vertx.createHttpServer();
 		http.requestHandler(new HTTPHandler(bridgeCfg));
 		SockJSServer sock = vertx.createSockJSServer(http);
-		sock.bridge(new JsonObject().putString("prefix", "/eventbus"),
-				bridgeCfg.getArray("inOK"), bridgeCfg.getArray("outOK"));
+		sock.bridge(new JsonObject().putString("prefix", "/eventbus"), bridgeCfg.getArray("inOK"),
+				bridgeCfg.getArray("outOK"));
 		// sock.setHook(new EventBusHook());
 		http.listen(bridgeCfg.getInteger("port"));
 	}
@@ -131,8 +124,7 @@ public class CoreVerticle extends BusModBase {
 				log.info("Deployed successfully: " + t);
 			} else {
 				reason = result.cause();
-				log.fatal("failed to deploy " + t + ": " + result.result(),
-						result.cause());
+				log.fatal("failed to deploy " + t + ": " + result.result(), result.cause());
 
 			}
 		}
