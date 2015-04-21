6f8eb8afdf61a92bafe9a3e16f1033563cc49d64
gerry <weirich@elexis.ch>
2015-04-21 18:19:06 +0200

    details

--- webelexis-base/src/main/resources/web/components/labview/ch-webelexis-labview.js
+++ webelexis-base/src/main/resources/web/components/labview/ch-webelexis-labview.js
@@ -2,15 +2,15 @@
  * This file is part of Webelexis
  * Copyright (c) 2015 by G. Weirich
  */
-
  define(['knockout', 'app/datetools', 'app/eb', 'app/config', 'text!tmpl/ch-webelexis-detailagenda.html', 'domReady!'], function (ko, dt, bus, cfg, html) {
 
-
-     function LabViewModel() {
+     function LabViewModel(){
+       var self=this
        
      }
      return {
        viewmodel: LabViewModel,
        template: html
      };
+
  });
