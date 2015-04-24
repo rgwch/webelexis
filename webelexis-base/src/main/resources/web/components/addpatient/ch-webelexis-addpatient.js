735f865ccc7e10797cd175692e233b0fc38137fa
Gerry <weirich@elexis.ch>
2015-04-24 18:53:58 +0200

    finished i18n of webelexis_addpatient

--- webelexis-base/src/main/resources/web/components/addpatient/ch-webelexis-addpatient.js
+++ webelexis-base/src/main/resources/web/components/addpatient/ch-webelexis-addpatient.js
@@ -10,15 +10,58 @@ define(['knockout', 'app/eb', 'app/config', 'text!tmpl/ch-webelexis-addpatient.h
     lastname: "Name",
     dob: "Geburtsdatum",
     mail: "E-Mail",
-    street:"Strasse",
-    zip:"Plz",
-    place:"Ort",
-    phone:"Telefon",
-    mobile:"Mobil",
-    insurance:"Krankenkasse",
-    insurance_no:"Versichertennummer",
-    password:"Passwort",
-    password_rep:"Passwort wiederholen"
+    street: "Strasse",
+    zip: "Plz",
+    place: "Ort",
+    phone: "Telefon",
+    mobile: "Mobil",
+    insurance: "Krankenkasse",
+    insurance_no: "Versichertennummer",
+    password: "Passwort",
+    password_rep: "Passwort wiederholen",
+    existing_pat: " Wenn Sie schon Patient/in bei uns sind, und jetzt nur ein Konto für die Online-Terminvergabe erstellen möchten, wählen Sie bitte diese Option.",
+    new_pat: " Wenn Sie noch nie bei uns waren, wählen Sie bitte diese Option und füllen Sie alle Felder aus.",
+    goto_login: ' Wenn Sie bereits ein Konto haben, können Sie direkt zum Login gehen.',
+    send_form: "Formular absenden",
+    privacy_header: "Datenschutzerklärung",
+    privacy_body: "Ihre Angaben werden nur zum Erstellen des Benutzerkontos und - falls noch nicht vorhanden - der Patientenakte verwendet, und um Sie nötigenfalls zu kontaktieren, falls wir einen Termin verschieben müssen. Unter keinen Umständen geben wir Daten an Dritte weiter. Wir werden Ihre Angaben auch nicht für Marketingzwecke missbrauchen. Sie können Jederzeit die Löschung Ihres Kontos und der hier eingetragenen Daten verlangen.",
+    warn_firstname:"Bitte geben Sie Ihren Vornamen ein.",
+    warn_lastname:"Bitte geben Sie Ihren Namen ein.",
+    warn_dob:"Bitte geben Sie Ihr Geburtsdatum ein (Format: t.m.y).",
+    warn_mail:"Die Mailadresse ist erforderlich (sie ist Ihr Login-Name).",
+    warn_pwd:"Das Passwort ist erforderlich (mindestens 5 Zeichen).",
+    warn_pwd_rep:"Bitte wiederholen Sie Ihr Passwort hier.",
+    warn_pwd_match:"Die beiden Passwörter stimmen nicht überein."
+
+  })
+
+  R.registerLocale("en",{
+    firstname:"first name",
+    lastname:"last name",
+    dob:"birthdate",
+    mail:"mail",
+    street:"street",
+    zip:"zip",
+    place:"place",
+    phone:"phone #",
+    mobile:"mobile phone",
+    insurance:"insurance",
+    insurance_no:"insurance number",
+    password:"password",
+    password_rep:"password (repeat)",
+    existing_pat:" If you are already a patient in our practice bot don't have an account, check here.",
+    new_pat:" If you are new to our practice, please check here and fill out all required fields",
+    goto_login:"If you have already an account, please go directly to 'Login'",
+    send_form:"transmit form",
+    privacy_header:"Declaration of privacy",
+    privacy_body:"We will use your details exclusively to create your account, and to contact you in case we need to change an appointment. We shall never disclose any of your details to third parties, unless required to do so by legal enforcement authorities.",
+    warn_firstname:"Please enter your first name.",
+    warn_lastname:"Please enter your last name.",
+    warn_dob:"Please enter your birthdate (european form: d.m.y).",
+    warn_mail:"The mail address is required (it will be your login name).",
+    warn_pwd:"A password of at least 5 characters is required.",
+    warn_pwd_rep:"Please enter your password here again.",
+    warn_pwd_match:"The passwords do not match."
   })
 
   R.setLocale(cfg.locale())
@@ -44,8 +87,8 @@ define(['knockout', 'app/eb', 'app/config', 'text!tmpl/ch-webelexis-addpatient.h
     self.labels = [R('firstname'), R('lastname'), R('dob'), R('mail'), R('street'), R('zip'),
       R('place'), R('phone'), R('mobile'), R('insurance'), R('insurance_no')
     ]
-    self.locale=function(name){
-      return R(name)
+    self.locale = function(name, elem) {
+      return R(name, elem)
     }
 
     self.send = function() {
@@ -117,14 +160,14 @@ define(['knockout', 'app/eb', 'app/config', 'text!tmpl/ch-webelexis-addpatient.h
 
       },
       messages: {
-        vorname: "Bitte geben Sie Ihren Vornamen an",
-        name: "Bitte geben Sie Ihren Namen an",
-        geburtsdatum: "Die Angabe des Geburtsdatums ist erforderlich (t.m.j)",
-        email: "Die korrekte Mail-Adresse wird als Login-Name benötigt.",
-        pass: "Ein mindestens 5 Zeichen langes Passwort muss eingetragen werden",
+        vorname: R('warn_firstname'),
+        name: R('warn_lastname'),
+        geburtsdatum: R('warn_dob'),
+        email: R('warn_mail'),
+        pass: R('warn_pwd'),
         pwdrep: {
-          required: "Bitte bestätigen Sie Ihr Passwort hier.",
-          equals: "Die Passwörter sind nicht identisch."
+          required: R('warn_pwd_rep'),
+          equals: R('warn_pwd_match')
         }
       }
     })
