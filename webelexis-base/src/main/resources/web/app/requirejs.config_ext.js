c3e824d9d5b5350dd1d65015d9927a623368b1aa
gerry <weirich@elexis.ch>
2015-04-26 10:40:26 +0200

    add require.config_ext.js

---
+++ webelexis-base/src/main/resources/web/app/requirejs.config_ext.js
@@ -0,0 +1,41 @@
+/*!
+ * This file is part of Webelexis
+ * (c) 2015 by G. Weirich
+ */
+
+/*
+This is a variant of the original require.config.js to load libraries from an external server.
+Use this, if your webelexis installation is located on a server with poor internet upstream speed.
+The application will the load only its own code from there, but fetch larger libraries from
+faster servers.
+*/
+var require = {
+  baseUrl: ".",
+  paths: {
+    "bootstrap": "http://webelexis.ch/lib/bootstrap.min",
+    "jquery": "http://webelexis.ch/lib/jquery.min",
+    "jquery-ui": "http://webelexis.ch/lib/jquery-ui",
+    "validate": "http://webelexis.ch/lib/jquery.validate.min",
+    "knockout": "http://webelexis.ch/lib/knockout",
+    "sockjs": "http://webelexis.ch/lib/sockjs.min",
+    "text": "http://webelexis.ch/lib/text",
+    "vertxbus": "http://webelexis.ch/lib/vertxbus",
+    "domReady": "http://webelexis.ch/lib/domReady",
+    "knockout-jqueryui": "http://webelexis.ch/lib/knockout-jqueryui",
+    "cookie": "http://webelexis.ch/lib/js.cookie-1.5.0.min",
+    "R": "http://webelexis.ch/lib/R",
+    "app": "app",
+    "tmpl": "tmpl"
+  },
+  shim: {
+    "bootstrap": {
+      deps: ["jquery"]
+    },
+    "knockout": {
+      deps: ["jquery"]
+    },
+    "validate": {
+      deps: ["jquery"]
+    }
+  }
+};
