e022fda0e3978eb982e5d5d751fa3fd560368ff0
Gerry <weirich@elexis.ch>
2015-04-28 09:53:39 +0200

    Handle if-modified-since request header correctly

--- webelexis-base/src/main/java/ch/webelexis/HTTPHandler.java
+++ webelexis-base/src/main/java/ch/webelexis/HTTPHandler.java
@@ -7,11 +7,13 @@ package ch.webelexis;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.InetSocketAddress;
-import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.TimeZone;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.MultiMap;
@@ -24,9 +26,9 @@ import org.vertx.java.core.json.JsonObject;
  * The handler for HTTP requests to the Webelexis server. If root (/) or
  * /index.html is requested, a Session is created, and a custom made File is
  * returned, which contains the SessionID, a unique STATE variable and the
- * Google ClientID of the server (if there is such a ClientID configured).
- * Paths are interpreted relative to the webroot of the Webelexis verticle. 
- * Requests to travel the directory tree upwards, are rejected.
+ * Google ClientID of the server (if there is such a ClientID configured). Paths
+ * are interpreted relative to the webroot of the Webelexis verticle. Requests
+ * to travel the directory tree upwards, are rejected.
  */
 
 public class HTTPHandler implements Handler<HttpServerRequest> {
@@ -34,41 +36,60 @@ public class HTTPHandler implements Handler<HttpServerRequest> {
 	JsonObject cfg;
 	EventBus eb;
 	// Date: Mon, 27 Apr 2015 16:51:46 GMT
-	SimpleDateFormat df=new SimpleDateFormat("E, dd M yyyy HH:mm:ss z");
+	SimpleDateFormat df = new SimpleDateFormat("E, dd MM yyyy HH:mm:ss z");
 
 	HTTPHandler(JsonObject cfg, EventBus eb) {
 		this.cfg = cfg;
 		this.eb = eb;
 		basePath = new File(cfg.getString("webroot"));
+		df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
 	}
 
 	@Override
 	public void handle(HttpServerRequest req) {
-		Date date=new Date();
+		Date date = new Date();
 		req.response().putHeader("Date", df.format(date));
 		req.response().putHeader("Server", "Webelexis");
 		if (req.path().equals("/") || req.path().equals("/index.html")) {
 			String rnd = UUID.randomUUID().toString();
-			InetSocketAddress remote=req.remoteAddress();
-			String IP="0.0.0.0";
-			if(remote!=null){
-				IP=remote.toString();
+			InetSocketAddress remote = req.remoteAddress();
+			String IP = "0.0.0.0";
+			if (remote != null) {
+				IP = remote.toString();
 			}
 			JsonObject sessionParams = new JsonObject().putString("clientID", cfg.getString("googleID"))
-						.putString("state", rnd).putString("remoteAddress", IP);
+					.putString("state", rnd).putString("remoteAddress", IP);
 			eb.send("ch.webelexis.session.create", sessionParams, new SessionHandler(req, rnd));
 		} else if (req.path().contains("..")) {
 			req.response().setStatusCode(404);
 			req.response().end();
 		} else {
-			File resr=new File(basePath, req.path());
-			Date lm=new Date(resr.lastModified());
-			req.response().putHeader("Last-Modified", df.format(lm));
-			if(req.path().endsWith(".css") || req.path().endsWith(".js")){
-				req.response().putHeader("Cache-Control", "max-age=86400");
-				
+			File resr = new File(basePath, req.path());
+			Date lm = new Date(resr.lastModified());
+			String reqDat = req.headers().get("if-modified-since");
+			boolean noSend = false;
+			if (reqDat != null) {
+				try {
+					Date reqDate = df.parse(reqDat);
+					if (!reqDate.before(lm)) {   // after or equal
+						noSend = true;
+					}
+				} catch (ParseException e) {
+					Logger.getGlobal().warning("could not parse reqDat");
+				}
 			}
-			req.response().sendFile(resr.getAbsolutePath());
+			req.response().putHeader("Last-Modified", df.format(lm));
+			if (req.path().endsWith(".css") || req.path().endsWith(".js")) {
+				req.response().putHeader("Cache-Control", "max-age=168800");
+			}
+			if (noSend) {
+				req.response().setStatusCode(304);
+				//req.response().setStatusMessage("not modified");
+				req.response().end();
+			} else {
+				req.response().sendFile(resr.getAbsolutePath());
+			}
 		}
 
 	}
@@ -78,18 +99,18 @@ public class HTTPHandler implements Handler<HttpServerRequest> {
 		String rnd;
 		MultiMap headers;
 		InetSocketAddress remoteAdress;
-		
+
 		SessionHandler(HttpServerRequest req, String rnd) {
 			this.req = req;
 			this.rnd = rnd;
-			headers =req.headers();
-			remoteAdress=req.remoteAddress();
+			headers = req.headers();
+			remoteAdress = req.remoteAddress();
 		}
 
 		@Override
 		public void handle(Message<JsonObject> msg) {
 			File in = new File(cfg.getString("webroot"), "index.html");
-			Date lm=new Date(in.lastModified());
+			Date lm = new Date(in.lastModified());
 			req.response().putHeader("Last-Modified", df.format(lm));
 			String cid = cfg.getString("googleID");
 			if (cid == null) {
@@ -98,16 +119,18 @@ public class HTTPHandler implements Handler<HttpServerRequest> {
 			Scanner scanner = null;
 			try {
 				scanner = new Scanner(in, "UTF-8");
-				String modified = scanner.useDelimiter("\\A").next().replaceAll("GUID",
-							msg.body().getString("sessionID")).replaceAll("GOOGLE_CLIENT_ID", cid).replaceAll(
-							"GOOGLE_STATE", rnd);
+				String modified = scanner.useDelimiter("\\A").next()
+						.replaceAll("GUID", msg.body().getString("sessionID")).replaceAll("GOOGLE_CLIENT_ID", cid)
+						.replaceAll("GOOGLE_STATE", rnd);
 
 				req.response().end(modified);
 			} catch (FileNotFoundException e) {
 				req.response().setStatusCode(404);
 				req.response().end();
 			} finally {
-				scanner.close();
+				if (scanner != null) {
+					scanner.close();
+				}
 			}
 
 		}
