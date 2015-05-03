6907eac4e7f5e784689017469af388346658d4a9
Gerry Weirich <weirich@elexis.ch>
2015-05-03 13:30:14 +0200

    send patient gender with lab values

--- webelexis-base/src/main/java/ch/webelexis/emr/LabResultSummaryHandler.java
+++ webelexis-base/src/main/java/ch/webelexis/emr/LabResultSummaryHandler.java
@@ -17,7 +17,9 @@ import ch.webelexis.Mapper;
 import ch.webelexis.ParametersException;
 
 /**
- * Quite simple: Just return all lab results of a given patient. For efficiency reasons, we fetch all interesting fields in a single join.
+ * Quite simple: Just return all lab results of a given patient. For efficiency
+ * reasons, we fetch all interesting fields in a single join.
+ * 
  * @author gerry
  *
  */
@@ -25,8 +27,8 @@ public class LabResultSummaryHandler implements Handler<Message<JsonObject>> {
 	Verticle v;
 	EventBus eb;
 	Logger log;
-	String[] fields = new String[] { "v.Datum", "v.ItemID", "li.titel", "v.Resultat", "v.Kommentar",
-				"li.kuerzel", "li.Gruppe", "li.prio", "li.RefMann", "li.RefFrauOrTx" };
+	String[] fields = new String[] { "v.Datum", "v.ItemID", "li.titel", "v.Resultat", "v.Kommentar", "li.kuerzel",
+			"li.Gruppe", "li.prio", "li.RefMann", "li.RefFrauOrTx", "k.Geschlecht" };
 
 	public LabResultSummaryHandler(Verticle server) {
 		v = server;
@@ -38,20 +40,20 @@ public class LabResultSummaryHandler implements Handler<Message<JsonObject>> {
 	public void handle(Message<JsonObject> externalRequest) {
 		Cleaner cl = new Cleaner(externalRequest);
 		try {
-			log.debug("LabResultSummaryHandler: Handling "+externalRequest.body().encode());
+			log.debug("LabResultSummaryHandler: Handling " + externalRequest.body().encode());
 			String patId = cl.get("patid", Cleaner.UID, false);
 			Mapper mapper = new Mapper(fields);
-			String query = "SELECT FIELDS FROM LABORWERTE as v, LABORITEMS as li where v.PatientID=? and v.ItemID=li.id and v.deleted='0' order by v.Datum";
+			String query = "SELECT FIELDS FROM LABORWERTE as v, LABORITEMS as li, KONTAKT as k where v.PatientID=? and v.ItemID=li.id and v.PatientID=k.id and v.deleted='0' order by v.Datum";
 
-			JsonObject jo = new JsonObject().putString("action", "prepared").putString("statement",
-						mapper.mapToString(query, "FIELDS")).putArray("values",
-						new JsonArray(new String[] { patId }));
+			JsonObject jo = new JsonObject().putString("action", "prepared")
+					.putString("statement", mapper.mapToString(query, "FIELDS"))
+					.putArray("values", new JsonArray(new String[] { patId }));
 			log.debug("sending message :" + jo.encodePrettily());
 			eb.send("ch.webelexis.sql", jo, new SqlResult(cl));
 
 		} catch (ParametersException e) {
 			e.printStackTrace();
-			log.error("Parameter error "+cl.toString());
+			log.error("Parameter error " + cl.toString());
 			cl.replyError("parameter error");
 		}
 	}
@@ -65,7 +67,7 @@ public class LabResultSummaryHandler implements Handler<Message<JsonObject>> {
 
 		@Override
 		public void handle(Message<JsonObject> sqlAnswer) {
-			log.debug("LabReuslt: Got answer from sql server: "+sqlAnswer.body().encodePrettily());
+			log.debug("LabReuslt: Got answer from sql server: " + sqlAnswer.body().encodePrettily());
 			JsonObject result = sqlAnswer.body();
 			cl.reply(result);
 		}
