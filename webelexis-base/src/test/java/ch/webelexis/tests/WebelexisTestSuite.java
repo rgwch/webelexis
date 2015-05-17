ae639c1b000295b7e3353317ac2a0951ef73accc
gerry <weirich@elexis.ch>
2015-05-17 16:27:00 +0200

    add coreverticle to TestSuite

--- webelexis-base/src/test/java/ch/webelexis/tests/WebelexisTestSuite.java
+++ webelexis-base/src/test/java/ch/webelexis/tests/WebelexisTestSuite.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+	CoreVerticleTest.class,
 	ConfigFileChecker.class,
 	AddPatientTest.class,
 	PublicAgendaListTest.class,
