d79ac598b6f838d42b6e2c5da2c63dd9918ad051
rgwch <weirich@elexis.ch>
2015-05-25 15:31:51 +0200

    konstext interprets Samdas markup correctly

--- webelexis-base/src/main/resources/web/app/editor.js
+++ webelexis-base/src/main/resources/web/app/editor.js
@@ -1,28 +1,32 @@
 /**
  * This file is part of Webelexis
+ * Copyright (c) 2015 by G. Weiricg
  */
 
-define(['knockout','tbw'],function(ko){
+/**
+ * custom binding (http://knockoutjs.com/documentation/custom-bindings.html) to embed the trumbowyg Editor in a knockout component.
+ */
+define(['knockout', 'tbw'], function (ko) {
 
-    var Editor = function(textObservable) {
-        var self=this;
+	var Editor = function (textObservable) {
+		var self = this;
 
-        ko.bindingHandlers.trumbowyg = {
+		ko.bindingHandlers.trumbowyg = {
 
-            init: function (element, valueAccessor, allBindings){
-                $(element).trumbowyg()
-                $(element).trumbowyg('html',"init")
-            },
+			init: function (element, valueAccessor, allBindings) {
+				$(element).trumbowyg()
+				$(element).trumbowyg('html', "init")
+			},
 
-            update: function(element, valueAccessor, allBindings){
-                $(element).trumbowyg('html',textObservable())
-            }
+			update: function (element, valueAccessor, allBindings) {
+				$(element).trumbowyg('html', textObservable())
+			}
 
-        }
-        self.getText=function(){
-            return self.content;
-        }
+		}
+		self.getText = function () {
+			return self.content;
+		}
 
-    }
-    return Editor;
+	}
+	return Editor;
 })
--- webelexis-base/src/main/resources/web/app/samdas.js
+++ webelexis-base/src/main/resources/web/app/samdas.js
@@ -3,32 +3,95 @@
  * Copyright (c) 2015 by G. Weirich
  */
 
-define({
+/*
+ functions to convert consultation entries between Elexis' Samdas-Format and HTML
+ */
 
-	plaintext: function (samdasText) {
-		var parser = new window.DOMParser()
-		var xml = parser.parseFromString(samdasText, "text/xml")
-		var textElem = xml.getElementsByTagName("text")[0]
-		if (textElem === undefined) {
-			return ""
-		} else {
-			return textElem.innerHTML
-		}
-	},
-	html     : function (samdasText) {
-		var parser = new window.DOMParser()
-		var xml = parser.parseFromString(samdasText, "text/xml")
-		var textElem = xml.getElementsByTagName("text")[0]
-		if (textElem === undefined) {
-			return ""
-		} else {
-			if (textElem.innerHTML) {
-				return textElem.innerHTML.replace(/\n/g, "<br>")
+define(['underscore'], function (_) {
+
+	var parser = new window.DOMParser()
+
+	var insert = function (text, start, len, css) {
+		var left = text.substr(0, start)
+		var middle = text.substr(start, len)
+		var right = text.substr(parseInt(start + len))
+		var ret = left + '<span class="' + css + '">' + middle + '</span>' + right
+		return ret
+	}
+	return {
+		plaintext: function (samdasText) {
+			var xml = parser.parseFromString(samdasText, "text/xml")
+			var textElem = xml.getElementsByTagName("text")[0]
+			if (textElem === undefined) {
+				return ""
 			} else {
-				return textElem
+				return textElem.innerHTML
+			}
+		},
+
+		html: function (samdasText) {
+			var xml = parser.parseFromString(samdasText, "text/xml")
+			var textElem = xml.getElementsByTagName("text")[0]
+			if (textElem === undefined) {
+				return ""
+			} else {
+				var ret = textElem.innerHTML
+				if (ret) {
+					var xrefs = _(xml.getElementsByTagName("xref")).map(function (m) {
+						return {
+							pos     : parseInt(m.getAttribute("from")),
+							len     : parseInt(m.getAttribute("length")),
+							provider: m.getAttribute("provider"),
+							xid     : m.getAttribute("id"),
+							type    : "xref"
+						}
+					})
+					var refs = xrefs.concat(_(xml.getElementsByTagName("markup")).map(function (m) {
+						return {
+							pos : parseInt(m.getAttribute("from")),
+							len : parseInt(m.getAttribute("length")),
+							type: m.getAttribute("type")
+						}
+					}))
+					var sorted = refs.sort(function (a, b) {
+						return a.pos - b.pos
+					})
+					var offset = 0
+
+					_.each(sorted, function (elem) {
+						switch (elem.type) {
+							case "emphasized":
+							case "bold":
+							{
+								ret = insert(ret, elem.pos + offset, elem.len, "bold")
+								offset += 22 + 4
+								break;
+							}
+							case "italic":
+							{
+								ret = insert(ret, elem.pos + offset, elem.len, "italic")
+								offset += 22 + 6
+								break;
+							}
+							case "underlined":
+							{
+								ret = insert(ret, elem.pos + offset, elem.len, "underline")
+								offset += 22 + 9
+								break;
+							}
+							case "xref":
+							{
+								ret = insert(ret, elem.pos + offset, elem.len, "xref")
+								offset += 22 + 4
+							}
+						}
+					})
+					return ret.replace(/\n/g, "<br />")
+				} else {
+					return textElem
+				}
+
 			}
 		}
-
 	}
-
 })
