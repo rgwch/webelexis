04fc8b687a7bc7a92c2d029c76ba971fe8d99c22
Gerry <weirich@elexis.ch>
2015-06-09 17:17:56 +0200

    continue

--- webelexis-client/src/js/app/addpatient/module.js
+++ webelexis-client/src/js/app/addpatient/module.js
@@ -2,70 +2,8 @@
  ** This file is part of Webelexis
  ** (c) 2015 by G. Weirich
  */
-define(['knockout', 'bus', 'app/config', 'text!tmpl/ch-webelexis-addpatient.html', 'app/datetools', 'jquery', 'validate'], function (ko, bus, cfg, html, dt, $) {
+define(['knockout', 'bus', 'app/config', 'text!tmpl/ch-webelexis-addpatient.html', 'app/datetools', 'i18n', 'jquery', 'validate'], function (ko, bus, cfg, html, dt, R, $) {
 
-  var Locale = {
-    de: {
-      heading: "Personalien eintragen",
-      firstname: "Vorname",
-      lastname: "Name",
-      dob: "Geburtsdatum",
-      mail: "E-Mail",
-      street: "Strasse",
-      zip: "Plz",
-      place: "Ort",
-      phone: "Telefon",
-      mobile: "Mobil",
-      insurance: "Krankenkasse",
-      insurance_no: "Versichertennummer",
-      password: "Passwort",
-      password_rep: "Passwort wiederholen",
-      existing_pat: " Wenn Sie schon Patient/in bei uns sind, und jetzt nur ein Konto für die Online-Terminvergabe erstellen möchten, wählen Sie bitte diese Option. Achten Sie bitte darauf, Namen und Geburtsdatum korrekt einzugeben, damit die Termine richtig zugeordnet werden können. Die E-Mail-Adresse wird der Beutzername sein, mit dem Sie sich später hier einloggen können.",
-      new_pat: " Wenn Sie noch nie bei uns waren, wählen Sie bitte diese Option und füllen Sie möglichst alle Felder aus. Fehlende Angaben können Sie selbstverständlich auch noch auf dem gewohnten Papier-Formular ergänzen, wenn Sie bei uns eintreffen.",
-      send_form: "Formular absenden",
-      privacy_header: "Datenschutzerklärung",
-      privacy_body: "Ihre Angaben werden nur zum Erstellen des Benutzerkontos und - falls noch nicht vorhanden - der Patientenakte verwendet, und um Sie nötigenfalls zu kontaktieren, falls wir einen Termin verschieben müssen. Unter keinen Umständen geben wir Daten an Dritte weiter. Wir werden Ihre Angaben auch nicht für Marketingzwecke missbrauchen. Sie können Jederzeit die Löschung Ihres Kontos und der hier eingetragenen Daten verlangen.",
-      warn_firstname: "Bitte geben Sie Ihren Vornamen ein.",
-      warn_lastname: "Bitte geben Sie Ihren Namen ein.",
-      warn_dob: "Bitte geben Sie Ihr Geburtsdatum ein (Format: t.m.y).",
-      warn_mail: "Die Mailadresse ist erforderlich (sie ist Ihr Login-Name).",
-      warn_pwd: "Das Passwort ist erforderlich (mindestens 5 Zeichen).",
-      warn_pwd_rep: "Bitte wiederholen Sie Ihr Passwort hier.",
-      warn_pwd_match: "Die beiden Passwörter stimmen nicht überein."
-
-    },
-    en: {
-      heading: "Enter personal informations",
-      firstname: "first name",
-      lastname: "last name",
-      dob: "birthdate",
-      mail: "mail",
-      street: "street",
-      zip: "zip",
-      place: "place",
-      phone: "phone #",
-      mobile: "mobile phone",
-      insurance: "insurance",
-      insurance_no: "insurance number",
-      password: "password",
-      password_rep: "password (repeat)",
-      existing_pat: " If you are already a patient in our practice bot don't have an account, check here.",
-      new_pat: " If you are new to our practice, please check here and fill out all required fields",
-      send_form: "transmit form",
-      privacy_header: "Declaration of privacy",
-      privacy_body: "We will use your details exclusively to create your account, and to contact you in case we need to change an appointment. We shall never disclose any of your details to third parties, unless required to do so by legal enforcement authorities.",
-      warn_firstname: "Please enter your first name.",
-      warn_lastname: "Please enter your last name.",
-      warn_dob: "Please enter your birthdate (european form: d.m.y).",
-      warn_mail: "The mail address is required (it will be your login name).",
-      warn_pwd: "A password of at least 5 characters is required.",
-      warn_pwd_rep: "Please enter your password here again.",
-      warn_pwd_match: "The passwords do not match."
-
-    }
-  }
-
-  var R = Locale[cfg.locale()]
 
   function AddPatientModel() {
     var self = this
@@ -82,11 +20,11 @@ define(['knockout', 'bus', 'app/config', 'text!tmpl/ch-webelexis-addpatient.html
       mobil: "",
       krankenkasse: "",
       versicherungsnummer: "",
-      pass: "",
+      pass: ""
     })
 
-    self.labels = [R.firstname, R.lastname, R.dob, R.mail, R.street, R.zip,
-      R.place, R.phone, R.mobile, R.insurance, R.insurance_no
+    self.labels = [R.t('m.add.firstname'), R.t('m.add.lastname'), R.t('m.add.dob'), R.t('m.add.mail'), R.t('m.add.street'), R.t('m.add.zip'),
+      R.t('m.add.place'), R.t('m.add.phone'), R.t('m.add.mobile'), R.t('m.add.insurance'), R.t('m.add.insurance_no')
     ]
     self.locale = function (name) {
       return R[name]
