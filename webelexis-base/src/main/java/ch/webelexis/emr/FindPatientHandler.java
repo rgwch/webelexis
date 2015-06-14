cfd532ca738e15d9a2688d814cc43ed769042acd
gerry <weirich@elexis.ch>
2015-06-14 20:31:55 +0200

    FindPatient

--- webelexis-base/src/main/java/ch/webelexis/emr/FindPatientHandler.java
+++ webelexis-base/src/main/java/ch/webelexis/emr/FindPatientHandler.java
@@ -5,26 +5,66 @@
 package ch.webelexis.emr;
 
 import ch.webelexis.Cleaner;
+import ch.webelexis.Mapper;
+import ch.webelexis.ParametersException;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
+
+import java.util.Iterator;
 
 /**
  * Created by gerry on 14.06.15.
  */
 public class FindPatientHandler implements Handler<Message<JsonObject>> {
     Verticle server;
+    Logger log;
+    EventBus eb;
 
+    final static String[] fields = {"Bezeichnung1", "Bezeichnung2", "Geburtsdatum", "geschlecht", "id"};
+    final static String sql = "SELECT FIELDS from KONTAKT where Bezeichnung1 like ? OR Bezeichnung2 like ? order by Bezeichnung1, Bezeichnung2";
     public FindPatientHandler(Verticle server) {
         this.server = server;
-
+        log = server.getContainer().logger();
+        eb = server.getVertx().eventBus();
     }
 
     @Override
     public void handle(Message<JsonObject> externalEvent) {
         Cleaner cl = new Cleaner(externalEvent);
-
+        try {
+            String expr = "%" + cl.get("expr", Cleaner.TEXT, false) + "%";
+            Mapper mapper = new Mapper(fields);
+            JsonObject jo = new JsonObject().putString("action", "prepared").putString("statement", mapper.mapToString(sql, "FIELDS"))
+                    .putArray("values", new JsonArray(new String[]{expr}));
+            log.debug("sending Query " + jo.encode());
+            eb.send("ch.webelexis.sql", jo, new Handler<Message<JsonObject>>() {
+                @Override
+                public void handle(Message<JsonObject> response) {
+                    JsonObject ans = response.body();
+                    if (ans.getString("status").equals("ok")) {
+                        JsonArray rows = ans.getArray("results");
+                        Iterator it = rows.iterator();
+                        JsonArray ret = new JsonArray();
+                        while (it.hasNext()) {
+                            Object[] row = (Object[]) it.next();
+                            JsonObject jo = mapper.mapToJson(row);
+                            ret.add(jo);
+                        }
+                        JsonObject result = new JsonObject().putString("status", "ok").putArray("result", ret);
+                        cl.reply(result);
+                    } else {
+                        cl.replyError(ans.getString("message"));
+                    }
+                }
+            });
+        } catch (ParametersException pex) {
+            cl.replyError(pex.getLocalizedMessage());
+        }
     }
 
 
