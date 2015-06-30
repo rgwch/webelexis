0b1fc8b97d56f30aa36f9e8541008f0568715944
gerry <weirich@elexis.ch>
2015-06-30 21:14:51 +0200

    migrate mock verticle

--- webelexis-server/src/test/TestCaptcha.java
+++ webelexis-server/src/test/TestCaptcha.java
@@ -1,5 +1,11 @@
+import ch.webelexis.Cleaner;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
+
+import java.io.IOException;
 
 /**
  * Created by gerry on 30.06.15.
@@ -7,7 +13,16 @@ import io.vertx.ext.unit.TestContext;
 public class TestCaptcha implements Handler<TestContext> {
 
   @Override
-  public void handle(TestContext testContext) {
-
+  public void handle(TestContext testContext){
+    try {
+      JsonObject config= Cleaner.createFromFile("TestCaptcha.json");
+      Vertx vertx= Vertx.vertx();
+      vertx.deployVerticle("MockVerticle",new DeploymentOptions().setConfig(config),run -> {
+        testContext.assertTrue(run.succeeded());
+      });
+    } catch (IOException e) {
+      e.printStackTrace();
+      testContext.fail("Error loading config");
+    }
   }
 }
