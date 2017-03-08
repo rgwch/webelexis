6dc68c710da0460ac1ac4b1f7b8085de1051fa50
gerry <weirich@elexis.ch>
2017-03-08 15:58:15 +0100

    datepicker

--- client/src/routes/intro/index.ts
+++ client/src/routes/intro/index.ts
@@ -2,22 +2,40 @@ import {FhirService} from '../../services/fhirservice';
 import {Patient, PatientFactory} from "../../models/patient";
 import {inject} from 'aurelia-framework'
 import {Config} from '../../config'
+import {Router} from 'aurelia-router';
 
-@inject(PatientFactory, FhirService, Config)
-export class Intro{
+
+@inject(PatientFactory, FhirService, Config, Router)
+export class Intro {
   selectedDate:Date
-  public searchexpr: string = '';
-  public patients: Array<Patient>
+  public searchexpr:string = '';
+  public patients:Array<Patient>
   private patientFactory
   private patientService;
-  private officeName="Webelexis"
+  private officeName = "Webelexis"
 
-  constructor(patientFactory: PatientFactory, patientService: FhirService, private cfg:Config) {
-    this.patientFactory=patientFactory
-    this.patientService=patientService
-    this.officeName=cfg.general.officeName
+  private agendaD = {
+    monthsFull: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    monthsShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    weekdaysFull: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+    weekdaysShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    today: 'Heute',
+    clear: 'Löschen',
+    close: 'Schließen',
+    firstDay: 1,
+    format: 'dddd, dd. mmmm yyyy',
+    formatSubmit: 'yyyy/mm/dd'
   }
 
+  constructor(patientFactory:PatientFactory, patientService:FhirService, private cfg:Config, private router:Router) {
+    this.patientFactory = patientFactory
+    this.patientService = patientService
+    this.officeName = cfg.general.officeName
+  }
+
+  goToAgenda(){
+    this.router.navigate('agenda')
+  }
   doSearch = function () {
     this.patientService.filterBy(this.patientFactory, "name", this.searchexpr).then(result => {
       if (result) {
