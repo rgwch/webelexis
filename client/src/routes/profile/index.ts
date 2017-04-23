82a364f65554a8927483bfc4aff34e47c9588ea2
gerry <weirich@elexis.ch>
2017-04-23 21:19:32 +0200

    added logout

--- client/src/routes/profile/index.ts
+++ client/src/routes/profile/index.ts
@@ -1,10 +1,37 @@
+import {autoinject} from 'aurelia-framework'
+import {Session} from '../../services/session'
+import {computedFrom} from "aurelia-framework";
+import {Router} from 'aurelia-router'
+
+@autoinject()
 export class Profile{
-  username="ha"
-  oldpwd="pwd"
-  newpwd="neuespwd"
-  repeatpwd="reppwd"
+  private user={}
+  private oldpwd:string=""
+  private newpwd:string=""
+  private repeatpwd:string=""
+
+  constructor(private session:Session, private router:Router){
+    this.user=this.session.getUser()
+  }
+
+  getUsername(){
+    if(this.user['emails'] instanceof Array){
+      return this.user['emails'][0].value
+    }else{
+      return this.user['emails']
+    }
+  }
+
+  @computedFrom('oldpwd','newpwd','repeatpwd')
+  get canChangePwd():boolean{
+    let result=this.oldpwd.length>0
+      && this.newpwd.length>0
+      && this.newpwd === this.repeatpwd
+    return result
+  }
 
   logout(){
-    alert("Log Out")
+    this.session.logout()
+    this.router.navigate("/")
   }
 }
