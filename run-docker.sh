5653b37d43475a32ad6d3263bc22f2b3c0929647
rgwch <weirich@elexis.ch>
2017-04-30 16:05:43 +0200

    preparing release 2.0.6

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker rm webelexis
-docker run -d -p 2017:2017 --name webelexis -v `pwd`/Janus/config.json:/usr/src/app/Janus/config.json rgwch/webelexis:2.0.5
+docker run -d -p 2017:2017 --name webelexis -v `pwd`/Janus/config.json:/usr/src/app/Janus/config.json rgwch/webelexis:2.0.6
