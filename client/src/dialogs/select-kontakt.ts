76aa0e63e127ee1d94e0f92898e042e303ac915c
rgwch <weirich@elexis.ch>
2018-10-28 19:09:23 +0100

    kontakt-selector

---
+++ client/src/dialogs/select-kontakt.ts
@@ -0,0 +1,51 @@
+import { KontaktType } from './../models/kontakt';
+import { DataSource } from './../services/datasource';
+import { DataService } from 'services/datasource';
+import { DialogController } from 'aurelia-dialog';
+import { autoinject, observable, valueConverter } from "aurelia-framework";
+import {BindingSignaler} from 'aurelia-templating-resources'
+
+import './select-kontakt.scss'
+
+@autoinject
+export class SelectKontakt{
+  @observable value;
+  found=[]
+  selection:string=""
+
+  private kontaktService:DataService;
+
+  constructor(private dc:DialogController, private ds:DataSource, private signaler: BindingSignaler){
+    this.kontaktService=ds.getService("kontakt")
+  }
+
+  valueChanged(newValue,oldValue){
+    this.kontaktService.find({query:{$find: this.value}}).then(result=>{
+      this.found=result.data
+    })
+  }
+
+  makeLabel(k:KontaktType){
+    return k.Bezeichnung1+" "+k.Bezeichnung2
+  }
+
+  select(k){
+    this.selection=k.id
+    this.signaler.signal('selected')
+    console.log(k)
+  }
+}
+
+/*
+  set item class according to selection Status (needs signal 'selected')
+*/
+@valueConverter('kontaktSelected')
+export class KontaktSelected{
+  toView(item,sel){
+    if(sel == item.id){
+      return "highlight"
+    }else{
+      return "listitem"
+    }
+  }
+}
