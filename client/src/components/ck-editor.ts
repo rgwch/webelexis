bcd4964f3a3754e357363ea5b064b4f430aa35a2
gerry <weirich@elexis.ch>
2018-11-04 21:11:18 +0100

    replace ck

--- client/src/components/ck-editor.ts
+++ client/src/components/ck-editor.ts
@@ -27,11 +27,31 @@ export class CKEditor {
       this.value = e.editor.getData();
     });
     editor.on('key',evt=>{
-      if(evt.data.keyCode==223){   // $
+      if(evt.data.domEvent.$.key=='$'){   // $
         const ed=evt.editor.getSelection()
         const range=ed.getRanges()[0]
-        const cursor=range.startOffset
-        console.log(cursor)
+        const ipos=range.endOffset
+        const text=range.endContainer.$.data
+        console.log(text)
+        const idx = text.lastIndexOf(' ') // -1 if at beginning of line
+          const word = text.substring(idx + 1, ipos)
+          console.log(word)
+          const replacement = this.callback(word)
+          console.log(replacement)
+
+          const r2=range.clone()
+          const nn=r2.getBoundaryNodes()
+          r2.setStart(nn.startNode,ipos-word.length)
+          r2.setEnd(nn.endNode,ipos)
+          r2.deleteContents(true)
+          r2.select()
+          evt.editor.insertText(replacement)
+          /*
+          const pos = change.position.getShiftedBy(word.length * -1)
+          range.start = pos
+          writer.remove(range)
+          writer.insertText(replacement, pos)
+          */
       }
       console.log(evt)
     })
