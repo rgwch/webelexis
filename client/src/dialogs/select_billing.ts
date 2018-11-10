134df197d8b61d59f6513e891788ec376445a4a7
gerry <weirich@elexis.ch>
2018-11-10 22:50:29 +0100

    div

--- client/src/dialogs/select_billing.ts
+++ client/src/dialogs/select_billing.ts
@@ -1,13 +1,50 @@
 import { DataSource, DataService } from './../services/datasource';
 import { DialogController } from 'aurelia-dialog';
-import { autoinject, useView, PLATFORM } from "aurelia-framework";
+import { autoinject, useView, PLATFORM, observable } from "aurelia-framework";
+import { EncounterType } from 'models/encounter';
 
 @autoinject
-export class SelectBilling{
-  tarmedService:DataService
+export class SelectBilling {
+  tarmedService: DataService
+  articleService: DataService
+  @observable position: string
+  encounter: EncounterType
+  billables=[]
+  a={
+    b:"c",
+    [this.position]:"d"
+  }
+  sql=(fld)=>{return {query: {[fld]: {$like: this.position+"%"}}}}
 
-  constructor(private dc:DialogController, private ds:DataSource){
-    this.tarmedService=ds.getService("tarmed")
+  activate(kons){
+    this.encounter=kons
+  }
+  positionChanged(newValue, oldValue) {
+    this.billables=[]
+    const tsql=this.sql("tx255")
+    
+    this.tarmedService.find({query: {$find: this.position,$enctr: this.encounter}}).then(result=>{
+      if(result.data && result.data.length){
+        this.billables=this.billables.concat(result.data)
+      }
+    })
+    this.articleService.find(this.sql("DSCR")).then(result=>{
+      if(result.data && result.data.length){
+        this.billables=this.billables.concat(result.data)
+      }
+    })
+  }
+  constructor(private dc: DialogController, private ds: DataSource) {
+    this.tarmedService = ds.getService("tarmed")
+    this.articleService = ds.getService("article")
+  }
+
+  makeLabel(elem){
+    if(elem.tx255){
+      return elem.tx255
+    }else{
+      return elem.DSCR
+    }
   }
 
 }
--- server/src/services/tarmed/tarmed.hooks.js
+++ server/src/services/tarmed/tarmed.hooks.js
@@ -1,9 +1,23 @@
-
+const findpos = async ctx => {
+  if (ctx.params.query && ctx.params.query.$find) {
+    const expr = ctx.params.query.$find
+    ctx.params.query.tx255 = { $like: expr + "%" }
+    if (ctx.params.query.$enctr) {
+      const enct = ctx.params.query.$enctr
+      ctx.params.query.GueltigVon = { $lte: enct.datum }
+      ctx.params.query.GueltigBis = { $gte: enct.datum }
+      // ctx.params.query.Law = {$or: ["KVG","UVG"]}
+      delete ctx.params.query.$enctr
+    }
+    delete ctx.params.query.$find
+  }
+  return ctx
+}
 
 module.exports = {
   before: {
     all: [],
-    find: [],
+    find: [findpos],
     get: [],
     create: [],
     update: [],
