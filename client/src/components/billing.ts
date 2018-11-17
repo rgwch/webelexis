46391122f2e698fa19392c3f9c22161ce0b1e445
rgwch <weirich@elexis.ch>
2018-11-17 22:23:30 +0100

    drag&drop

--- client/src/components/billing.ts
+++ client/src/components/billing.ts
@@ -1,13 +1,16 @@
+import { BillingModel } from './../models/billings-model';
 import { SelectBilling } from '../dialogs/select_billing';
 import { PLATFORM,bindable, autoinject, useView } from "aurelia-framework";
 import { BillingsManager } from "models/billings-model";
 import { DialogService } from "aurelia-dialog";
 
 @autoinject
-@useView(PLATFORM.moduleName("./billing.pug"))
+// @useView(PLATFORM.moduleName("./billing.pug"))
 export class Billing{
   @bindable kons;
   billings
+  billingdiv
+  displaybillings=true
 
   constructor(private bm:BillingsManager, private ds:DialogService){}
 
@@ -18,11 +21,14 @@ export class Billing{
   }
 
   addBilling(){
+    /*
     this.ds.open({viewModel: SelectBilling, model: this.kons}).whenClosed(result=>{
       if(!result.wasCancelled){
         console.log("result")
       }
     })
+    */
+   this.displaybillings=!this.displaybillings
   }
 
   dragOver(event){
@@ -38,10 +44,11 @@ export class Billing{
     const data = event.dataTransfer.getData("text");
     this.bm.getBillable(data).then(billable=>{
       this.bm.createBilling(billable,this.kons,1).then(billing=>{
-        this.billings.push(billing)
+        this.displaybillings=false
+        this.billings.push(new BillingModel(billing))
+        this.displaybillings=true
       })
     })
-    console.log(event)
     return true
   }
 }
