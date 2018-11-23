befbe484f15fe2d8597a983dd82334a85aa77b9e
gerry <weirich@elexis.ch>
2018-11-23 07:04:40 +0100

    details

--- server/src/services/lucinda/lucinda.class.js
+++ server/src/services/lucinda/lucinda.class.js
@@ -9,21 +9,25 @@ class Service {
     this.options = options || {};
   }
 
+  /**
+   * find matching documents.
+   * @param {} params - query: A lucene-style query string
+   */
   async find(params) {
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
       request({
         method: "POST",
-        url: this.options.url+"query",
+        url: this.options.url + "query",
         json: true,
         body: params.query
-      },(err,res)=>{
-        if(err) reject(err)
-        if(res){
-          if(res.statusCode==204){
+      }, (err, res) => {
+        if (err) reject(err)
+        if (res) {
+          if (res.statusCode == 204) {
             resolve([])
-          }else if(res.statusCode==200){
+          } else if (res.statusCode == 200) {
             resolve(res.body)
-          }else{
+          } else {
             reject("bad result")
           }
         }
@@ -31,6 +35,12 @@ class Service {
     })
   }
 
+  /**
+   * Load a document by id
+   * Returns: The binary contents of the document
+   * @param {} id
+   * @param {*} params
+   */
   get(id, params) {
     if (id == "info") {
       return new Promise((resolve, reject) => {
@@ -59,14 +69,27 @@ class Service {
     }
   }
 
+  /**
+   * Add a new document to the index
+   * @param {} data
+   * {
+   *  payload: base64-encoded binary content of the document
+   *  filename: If given, store the document. Otherwise index only
+   *  concern: If given, subfolder to store (e.g. patient name)
+   *  any_attribute: "any value" - arbitrary user defined metadata
+   * }
+   * @param {*} params
+   * @returns: a result with statusCode (201 for ok), a status with "ok" and the _id.
+   */
   create(data, params) {
     if (Array.isArray(data)) {
       return Promise.all(data.map(current => this.create(current, params)));
     }
     return new Promise((resolve, reject) => {
+      const endpoint = data.filename ? "addfile" : "index"
       request({
         method: "POST",
-        uri: this.options.url + "addfile",
+        uri: this.options.url + endpoint,
         body: data,
         json: true
       }, (err, result) => {
@@ -81,12 +104,35 @@ class Service {
 
   }
 
-  async update(id, data, params) {
-    return data;
+  /**
+   * Update (i.e. reindex) an existing document. Only metadata are updated, not the document itself
+   * @param {*} id: id of the document to reindex
+   * @param {*} data @see create
+   * @param {*} params
+   */
+  update(id, data, params) {
+    return new Promise((resolve, reject) => {
+      request({
+        method: "POST",
+        uri: this.options.url + "update",
+        body: data,
+        json: true
+      }, (err, result) => {
+        if (err) reject(err)
+        if (result) {
+          if (result.statusCode == 202) {
+            return result.body
+          } else {
+            reject("error "+result.statusCode.toString())
+          }
+        }
+      })
+    })
+
   }
 
   async patch(id, data, params) {
-    return data;
+    return await this.update(id,data,params);
   }
 
   remove(id, params) {
@@ -95,7 +141,7 @@ class Service {
         if (err) reject(err)
         if (res) {
           if (res.statusCode == 200) {
-            if (typeof(res.body)=='string') {
+            if (typeof (res.body) == 'string') {
               resolve(JSON.parse(res.body))
             } else {
               resolve(res.body)
---
+++ server/src/services/lucinda/whatisit.md
@@ -0,0 +1,50 @@
+## Lucinda connector
+
+This Service connects Webelexis with a [Lucinda](https://github.com/rgwch/lucinda) Server.
+
+### Preparation
+
+#### 1. install and run a Lucinda instance on a machine reachable by webelexis
+
+* create a config.cfg
+
+      # Language for the parser/tokenizer/indexer
+      default_language=de
+
+      # Use the REST interface
+      rest_use=yes
+      rest_port=2016
+
+      # base directory for directories not named below
+      fs_basedir=/srv/lucinda/store
+
+      # where to keep the index. Make sure to backup this directory regularly
+      fs_indexdir=/srv/lucinda/store/index
+
+      # directory or comma separated list of directories to watch for changes and apply to index
+      fs_watch=/srv/lucinda/store/inbox
+
+      # Base directory for adding files programmatically (should be one of the directories named in 'watchdirs'
+      # fs_import=/srv/lucinda/store/inbox
+
+      # OCR to use for image files. At this time, only tesseract is supported. Must be installed separately
+      ocr=tesseract
+
+* run lucinda:
+
+      java -jar lucinda-server-2.0.0-SNAPSHOT.jar
+
+#### 2. tell Webelexis the connection details:
+
+In `server/config/default.json` add:
+
+       "lucinda":{
+          "url": "http://localhost:2016/lucinda/2.0/"
+        }
+
+(Or whatever matches your setup)
+
+#### 3. Run tests
+
+in `server/test/services/lucinda.test.js` change 'xdescribe'  to 'describe', and run `npm test`
+
