7d25a5c56c608842701456d1d06a8c3c6a48cdd9
gerry <weirich@elexis.ch>
2018-12-14 20:32:03 +0100

    hard delete for local remove

--- server/src/hooks/softdelete.js
+++ server/src/hooks/softdelete.js
@@ -1,12 +1,16 @@
 
 
-module.exports = function (options={}) {
+module.exports = function (options = {}) {
   return ctx => {
-    ctx.data=ctx.data || {}
-    ctx.data.deleted="1"
-    return ctx.service.patch(ctx.id, ctx.data, ctx.params).then(deleted => {
-      ctx.result=deleted
+    if (ctx.params.provider) {
+      ctx.data = ctx.data || {}
+      ctx.data.deleted = "1"
+      return ctx.service.patch(ctx.id, ctx.data, ctx.params).then(deleted => {
+        ctx.result = deleted
+        return ctx
+      })
+    } else {
       return ctx
-    })
+    }
   }
 }
