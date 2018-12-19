0eeb07c381943471affd9b397d06a1719cda9960
gerry <weirich@elexis.ch>
2018-12-19 13:23:29 +0100

    div

--- client/src/models/briefe-model.ts
+++ client/src/models/briefe-model.ts
@@ -23,7 +23,7 @@ export interface BriefType extends ElexisType {
 export class BriefManager {
   briefService: DataService
 
-  constructor(private ds: DataSource, private we: WebelexisEvents, private dt:DateTime) {
+  constructor(private ds: DataSource, private we: WebelexisEvents, private dt: DateTime) {
     this.briefService = ds.getService('briefe')
   }
 
@@ -41,13 +41,17 @@ export class BriefManager {
   replaceFields(template: string, brief: BriefType, fields?: Array<{ field: string, replace: string }>) {
     const fieldmatcher = /\[\w+\.\w+\]/ig
     for (const f of fields) {
-      template = template.replace("["+f.field+"]", f.replace)
+      if (!f.replace) {
+        f.replace = ""
+      }
+      template = template.replace("[" + f.field + "]", f.replace)
+
     }
 
     const compiled = template.replace(fieldmatcher, field => {
       const stripped = field.substring(1, field.length - 1)
       const [element, attribute] = stripped.split(".")
-      let replacement:string
+      let replacement: string
       switch (element.toLowerCase()) {
         case "adressat":
         case "addressee": replacement = brief.destid ? brief.destid[attribute] : null; break;
@@ -62,8 +66,8 @@ export class BriefManager {
         }
       }
       if (replacement) {
-        if(replacement.match(/[0-9]{8,8}/)){
-          replacement=this.dt.ElexisDateToLocalDate(replacement)
+        if (replacement.match(/[0-9]{8,8}/)) {
+          replacement = this.dt.ElexisDateToLocalDate(replacement)
         }
         return replacement
       } else {
