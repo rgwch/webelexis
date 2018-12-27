b65da3b5c6b19113d4c2600edc7271303dd0af78
gerry <weirich@elexis.ch>
2018-12-27 22:12:12 +0100

    auto-modify database

--- server/src/knex.js
+++ server/src/knex.js
@@ -11,6 +11,9 @@
  */
 
 const knex = require('knex');
+const logger=require('./logger')
+const fs=require('fs')
+const path=require('path')
 
 module.exports = function (app) {
   // undocument exactly one of the following three lines
@@ -20,6 +23,32 @@ module.exports = function (app) {
   const db = knex({ client,
       connection: app.get("userconfig").elexisdb || connection,
       pool: { max: 50 } });
-
+  db('config').select('wert').where('param','webelexis').then(result=>{
+    if(result && result.length>0){
+      console.log("Found Webelexis Version %s",result[0].wert)
+    }else{
+      logger.warn("webelexis config entry not found")
+      const script=fs.readFileSync('./modify_elexis.sql',"utf-8")
+      const statements=script.split(";")
+      const execs=[]
+      for(const stm of statements){
+        execs.push(db.raw(stm.replace(/[\n\r]/,"")).catch(err=>{
+          logger.warn("statement failed: %s",err)
+        }))
+      }
+      Promise.all(execs).then(res=>{
+        const version=app.get('version')
+        db('config').insert({param:'webelexis',wert:version}).then(r=>{
+          logger.info("script finished")
+        }).catch(err=>{
+          logger.error("could not insert version %s",err)
+        })
+      })
+     
+    }
+  }).catch(err=>{
+    logger.error("Can't connect do database: %s ",err)
+    process.exit(42)
+  }) 
   app.set('knexClient', db);
 };
