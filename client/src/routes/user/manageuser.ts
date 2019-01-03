b823d29c381a2244f9f4e11e6d3b45a3b1ba504a
gerry <weirich@elexis.ch>
2019-01-03 16:23:38 +0100

    users and roles

--- client/src/routes/user/manageuser.ts
+++ client/src/routes/user/manageuser.ts
@@ -2,14 +2,16 @@ import { autoinject, useView, PLATFORM } from "aurelia-framework";
 import { UserManager, UserType } from "models/user";
 import {CommonViewer, ViewerConfiguration} from 'components/commonviewer'
 import { EventAggregator } from "aurelia-event-aggregator";
+import env from 'environment'
 
 @autoinject
 @useView(PLATFORM.moduleName('./manageuser.pug'))
 
 export class Manageuser{
+  eamessage="usermgr:selected"
   vc: ViewerConfiguration={
     dataType: 'usr',
-    selectMsg: "usermgr:selected",
+    selectMsg: this.eamessage,
     title: 'Anwender',
     getLabel: obj=>obj.email,
     searchFields: [{
@@ -19,9 +21,20 @@ export class Manageuser{
     }]
   }
   user: UserType
+  roles 
+  rolenames:string[]
+  hasroles
   constructor(private userManager:UserManager, private ea:EventAggregator){
-    this.ea.subscribe("usrmgr:selected",user=>{
+    this.ea.subscribe(this.eamessage,user=>{
       this.user=user 
     }) 
+    this.roles=env.metadata.roles
+    this.rolenames=Object.keys(this.roles)
+  }
+}
+
+export class KeysValueConverter {
+  toView(obj) {
+    return Reflect.ownKeys(obj);
   }
 }
