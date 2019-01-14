0d3832ec56d97697f4d721a3326baddb15cbe16a
gerry <weirich@elexis.ch>
2019-01-14 16:17:23 +0100

    renew jwt secret on start

--- server/src/authentication.js
+++ server/src/authentication.js
@@ -7,10 +7,29 @@
 const authentication = require('@feathersjs/authentication');
 const jwt = require('@feathersjs/authentication-jwt');
 const local = require('@feathersjs/authentication-local');
-
+const crypto=require('crypto')
+const os=require('os')
+const fs=require('fs')
 
 module.exports = function (app) {
   const config = app.get('authentication');
+  const hasher=crypto.createHash('sha256')
+  hasher.update(Math.random().toString())
+  hasher.update(os.uptime().toString())
+  hasher.update(new Date().toString())
+  const load=JSON.stringify(os.loadavg())
+  hasher.update(load)
+  hasher.update(os.freemem().toString())
+  hasher.update(JSON.stringify(os.networkInterfaces()))
+  const temp=os.tmpdir()
+  hasher.update(temp)
+  fs.readdirSync(temp).forEach(f=>{
+    hasher.update(f)
+  })
+  const secret_of_the_day=hasher.digest('hex')
+
+  /* We create a new secret with every launch */
+  config.secret=secret_of_the_day
 
   // Set up authentication with the secret
   app.configure(authentication(config));
