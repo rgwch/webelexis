af8b95a9bb6627187eb5b1c049edffd6f211af7c
gerry <weirich@elexis.ch>
2019-01-18 22:02:08 +0100

    detail

--- client/src/views/encounters-by-date.ts
+++ client/src/views/encounters-by-date.ts
@@ -1,8 +1,13 @@
+/********************************************
+ * This file is part of Webelexis           *
+ * Copyright (c) 2019 by G. Weirich         *
+ * License and Terms see LICENSE            *
+ ********************************************/
+
 import { PatientType } from "./../models/patient";
 import { CaseType } from "./../models/case";
 import { Patient } from "models/patient";
 import { autoinject, observable } from "aurelia-framework";
-import { DataSource, DataService } from "services/datasource";
 import * as moment from "moment";
 import { EncounterType, EncounterManager } from "models/encounter-model";
 import { EventAggregator } from "aurelia-event-aggregator";
@@ -12,8 +17,8 @@ import { BillingModel } from "models/billings-model";
 @autoinject
 export class EncountersByDate {
   protected enc;
-  protected dateFrom = "2019-01-01";
-  protected dateUntil = "2019-01-31";
+  protected dateFrom = moment().format("YYYY-DD-MM")
+  protected dateUntil = this.dateFrom
   protected total: number
   protected count: number
 
