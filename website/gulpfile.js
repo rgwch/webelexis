b074b516f773d8646c06beb9c655affa80e4cd42
Gerry Weirich <gerry@Gerrys-MBP.fritz.box>
2019-02-27 06:38:43 +0100

    gulp based website

---
+++ website/gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require("gulp")
+const sass = require("gulp-sass")
+const pug = require("gulp-pug")
+const browserSync = require("browser-sync").create()
+const minifyCss = require("gulp-clean-css")
+const rimraf = require('rimraf')
+
+const dest = "target"
+
+const copy = () => {
+  return gulp.src("img/*").pipe(gulp.dest(dest))
+}
+
+const stylesTask = cb => {
+  gulp
+    .src("*.scss")
+    .pipe(sass())
+    .pipe(minifyCss())
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.reload({ stream: true }))
+  cb()
+}
+
+const pugTask = () => {
+  return gulp
+    .src(["*.pug", "!layout.pug"])
+    .pipe(pug())
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.reload({ stream: true }))
+}
+
+const clean = cb => {
+  rimraf(dest, cb)
+}
+const run = cb => {
+  browserSync.init({
+    server: {
+      baseDir: dest,
+    },
+    port: 2345
+  })
+  cb()
+}
+
+const watch = cb => {
+  gulp.watch("*.pug", pugTask)
+  gulp.watch("*.scss", stylesTask)
+  cb()
+}
+
+module.exports = {
+  default: gulp.series(gulp.parallel(stylesTask, pugTask, copy), watch, run),
+  watch: watch,
+  clean: clean
+}
---
+++ website/readme.md
@@ -0,0 +1,11 @@
+# Creating the website:
+
+````
+cd webelexis/website
+npm install
+npx gulp
+````
+
+# Deployment:
+
+Copy contents of 'target'-directory to the web server root.
