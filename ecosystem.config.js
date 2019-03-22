38230ac88d4486f2ffade7e5d465682daa36e81c
gerry <weirich@elexis.ch>
2019-03-22 18:24:11 +0100

    added pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps : [{
+    name: 'serv',
+    script: 'src/index.js',
+    cwd: 'server',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
+    env_dockered: {
+      NODE_ENV: 'dockered'
+    }
+  },{
+    name: "self",
+    script: "bin/www",
+    cwd: 'selfservice',
+    instances: 1,
+    autorestart: true,
+    watch: false
+  }],
+
+};
+
