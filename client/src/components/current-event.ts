a8084306df8e8ae2cf99312fab6924c2650b370c
rgwch <weirich@elexis.ch>
2020-04-11 21:45:08 +0200

    introduced scss

--- client/src/components/current-event.ts
+++ client/src/components/current-event.ts
@@ -1,6 +1,6 @@
 import { bindable } from 'aurelia-framework';
 import { IEvent } from 'models/event-model';
-import './components.css'
+import './components.scss'
 
 export class CurrentEvent{
   @bindable event:IEvent
