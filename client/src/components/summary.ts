92c9da2c9402907f4ad6e3b1b80352ee745bb530
gerry <weirich@elexis.ch>
2020-04-19 11:10:42 +0200

    headers

--- client/src/components/summary.ts
+++ client/src/components/summary.ts
@@ -1,20 +1,33 @@
-import { LogManager } from 'aurelia-framework';
-import { IPatient } from './../models/patient-model';
-import { bindable } from 'aurelia-framework';
+import { LogManager, autoinject, bindable, computedFrom } from 'aurelia-framework';
+import { IPatient, PatientManager } from './../models/patient-model';
 
-const log=LogManager.getLogger("Summary View")
+const log = LogManager.getLogger("Summary View")
 
+@autoinject
+export class Summary {
+  @bindable patient: IPatient
+  kontaktdata: boolean = false
 
-export class Summary{
-  @bindable patient:IPatient
-  kontaktdata:boolean = false
-   
-  attached(){
+  constructor(private pam: PatientManager) {
+
+  }
+
+  toggleKontaktdata(){
+    this.kontaktdata=!this.kontaktdata
+  }
+  
+  attached() {
     // log.info("Summary attached: "+this.patient)
   }
 
-  patientChanged(old,newp){
+  patientChanged(old, newp) {
     // log.info("Change patient from "+(old ? old.bezeichnung1 : old)+" to "+(newp ? newp.bezeichnung1 : newp))
   }
 
+  @computedFrom('patient')
+  get label() {
+    if (this.patient) {
+      return this.pam.getLabel(this.patient)
+    }
+  }
 }
--- client/src/models/patient-model.ts
+++ client/src/models/patient-model.ts
@@ -1,13 +1,33 @@
 import { ObjectManager } from './object-manager';
 import { IKontakt, KontaktManager } from './kontakt-model';
 import {ISticker} from './sticker-manager'
+import {autoinject} from 'aurelia-framework'
+import * as moment from 'moment'
 
 export interface IPatient extends IKontakt{
+  patientnr: string
 }
 
+@autoinject
 export class PatientManager extends ObjectManager{
-    constructor(){
+    constructor(private km:KontaktManager){
       super('patient')
     }
 
+    getLabel(pat: IPatient){
+      const lbl=this.km.getLabel(pat)+" ("+this.getAge(pat)+") "
+      return lbl+" ["+pat.patientnr+"]"
+    }
+
+    getAge(pat: IPatient){
+      const now=moment()
+      const bd=moment(pat.geburtsdatum,"YYYYMMDD")
+      const age=now.diff(bd,'years')
+      if(age<2){
+        const mo=now.diff(bd,'months')
+        return age+" "+mo+"/12"
+      }else{
+        return age
+      }
+    }
 }
