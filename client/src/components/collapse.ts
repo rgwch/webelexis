4865cce7f7c60e813576d33285d64677924e9b03
rgwch <weirich@elexis.ch>
2020-04-21 10:43:50 +0200

    animation

--- client/src/components/collapse.ts
+++ client/src/components/collapse.ts
@@ -1,18 +1,31 @@
-import { bindable } from 'aurelia-framework';
+import { autoinject, bindable } from 'aurelia-framework';
+import { Animator } from 'aurelia-templating'
+import './collapse.scss'
 
-export interface IAction{
-  exec: (obj)=>void
+export interface IAction {
+  exec: (obj) => void
   name: string
   icon: string
 }
 
+@autoinject
+export class Collapse {
+  isExpanded: boolean = false
+  collapsible: HTMLElement
+  @bindable actions: Array<IAction>
 
-export class Collapse{
-  isExpanded:boolean=false
-  @bindable actions:Array<IAction>
+  constructor(private animator: Animator) { }
 
-  toggle(){
-    this.isExpanded=!this.isExpanded
+  toggle() {
+    if (!this.isExpanded) {
+      this.isExpanded = true
+      this.animator.animate(this.collapsible, 'opening')
+
+    } else {
+      return this.animator.animate(this.collapsible, 'closing').then(() => {
+        this.isExpanded = false        
+      })
+    }
     console.log("toggle")
   }
 }
