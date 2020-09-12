b0ed26a616afb999bc48b198fddf18da082ff7f4
rgwch <rgw@elexis.ch>
2020-09-12 11:56:55 +0200

    copied favico

--- server/src/app.js
+++ server/src/app.js
@@ -46,7 +46,7 @@ app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.raw({ inflate: true, limit: "50mb", type: "application/octet-stream" }))
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 // Host the public folder
 app.use('/', express.static(app.get('public')));
