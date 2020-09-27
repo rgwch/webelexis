1d32408eaa86804832444a39f22c6a66086d3d61
rgwch <weirich@elexis.ch>
2020-09-27 12:46:50 +0200

    render doc

--- server/src/services/lucinda/lucinda.service.js
+++ server/src/services/lucinda/lucinda.service.js
@@ -4,12 +4,23 @@ const hooks = require('./lucinda.hooks');
 
 module.exports = function (app) {
 
-  const options=app.get("lucinda")
+  const options = app.get("lucinda")
   // Initialize our service with any options it requires
   app.use('/lucinda', createService(options));
 
   // Get our initialized service so that we can register hooks
   const service = app.service('lucinda');
+  app.get("/lucindadoc/:id", async (req, res) => {
+    const doc = await service.get(req.params.id)
+    res.set({
+      "Content-Type": "application/pdf",
+      "Content-length": doc.length
+    })
+    res.status(200)
+    res.send(doc)
+    res.end()
+  })
+
 
   service.hooks(hooks);
 };
