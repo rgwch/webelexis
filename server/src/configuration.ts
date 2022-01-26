7c8a2f7bed5fefee9d2c2129faf01206735e4075
rgwch <weirich@elexis.ch>
2022-01-26 20:06:25 +0100

    rewrites

---
+++ server/src/configuration.ts
@@ -0,0 +1,180 @@
+/**
+ * Settings example for the webelexis server
+ * please move this file to settings.js and
+ * change contents to match your system.
+ *
+ * For a Docker deployment, move this file to your
+ * data directory.
+ */
+export const config = {
+  /* if testing is true, webelexis will create some testusers on startup and allow to login without a password. */
+  testing: true,
+  /* The name to show on the browser tab */
+  sitename: "Praxis Webelexis",
+  /* Admin's mail is needed for registering new users and for lost password retrieval */
+  admin: "someone@webelexis.ch",
+  /* If the user database is empty, create an admin and assign this pasword. Make sure to remove the line
+     after the first start. */
+  adminpwd: "topsecret",
+  /* create a proxy server at :4040 for appointment-self-service requests */
+  self_service: true,
+  /* A list of mandators and metadata for them. Metadata are used in templates. You can define whatever you want
+     and refer to it e.g. in 'briefe' templates. See data/sample-docbase/templates/rezept.pug for an example. */
+  mandators: {
+    default: {
+      name: "Dr. med. Dok Tormed",
+      subtitle: "Facharzt für Webelexik",
+      street: "Hinterdorf 17",
+      place: "9999 Webelexikon",
+      phone: "555 55 55",
+      email: "doc@webelexis.org",
+      zsr: "G088113",
+      gln: "123456789012"
+    }
+  },
+  /* The place to store templates and documents */
+  docbase: "../data/sample-docbase",
+  /* connection to the database */
+  elexisdb: {
+    host: "localhost",
+    database: "elexis",
+    user: "praxis",
+    password: "topsecret",
+  },
+  /* We need an SMTP host to send mails for lost password retrieval */
+  smtp: {
+    host: "some.smpt.host",
+    port: 587,
+    user: "smtpuser",
+    pwd: "smtppassword"
+  },
+  /* Definition of the document store. Fallback is always
+    storage in the file system */
+  lucinda: {
+    url: "http://localhost:2016/lucinda/2.0"
+  },
+  /**
+   * Definition of default presets for appointments. Only needed if Webelexis
+   * runs from scratch i.e. without a previously installed and configured Elexos.
+   */
+  agenda: {
+    resources: ["Arzt", "MPA"],
+    daydefaults: `FS1~#<ASa=A0000-0900
+1200-2359~#<ADo=A0000-0800
+1200-1300
+1700-2359~#<AFr=A0000-0800
+1200-1300
+1700-2359~#<AMi=A0000-0800
+1300-2359~#<ADi=A0000-0900
+1300-1400
+1800-2359~#<AMo=A0000-0800
+1200-1300
+1700-2359~#<ASo=A0000-2359`,
+    termintypdefaults: ["Frei", "Reserviert", "Normal"],
+    terminstatedefaults: ["-", "geplant", "eingetroffen", "fertig", "abgesagt"],
+    typcolordefaults: {
+      Reserviert: "000000",
+      Frei: "80ff80",
+      Normal: "ff8040"
+    },
+    statecolordefaults: {
+      geplant: "ff8000",
+      eingetroffen: "ff0000",
+      fertig: "008000",
+      abgesagt: "e5e5e5"
+    },
+    timedefaults: {
+      Reserviert: 30,
+      Frei: 30,
+      Normal: 30
+    }
+  },
+  /**
+   * Settings for the self-service scheduling
+   */
+  schedule: {
+    minDuration: 30,
+    terminTyp: "Internet",
+    resource: "Arzt",
+    maxPerDay: 4,
+    sitename: "Praxis Webelexis",
+    siteaddr: "Hintergasse 17, 9999 Webelexikon",
+    sitephone: "555-55 55 55",
+    sitemail: "team@webelexis.ch"
+  },
+  /**
+   * Preset for Fall settings
+   */
+
+  fall: {
+    fallgesetz: "KVG",
+    fallgrund: "Krankheit",
+    fallbezeichnung: "Allg.",
+  }
+}
+export const roles = {
+  guest: {
+    id: "guest",
+    label: "Gast",
+    descr: "a person not known to the system"
+  },
+  Patient: {
+    id: "patient",
+    label: "Patient",
+    descr: "Someone who can see their own data only"
+  },
+  user: {
+    id: "user",
+    label: "Anwender",
+    descr: "a person known to the system"
+  },
+  external: {
+    id: "user_external",
+    label: "externer Anwender",
+    descr: "A user who can access remotely"
+  },
+  mpa: {
+    id: "assistant",
+    label: "MPA",
+    descr: "a person working with the practice"
+  },
+  agenda: {
+    id: "appnt",
+    label: "Agenda",
+    descr: "someone who may manage appointments"
+  },
+  doc: {
+    id: "doctor",
+    label: "Arzt",
+    descr: "A doctor"
+  },
+  executive: {
+    id: "executive_doctor",
+    label: "Leitender Arzt",
+    desc: "A doctor with administrative functions"
+  },
+  admin: {
+    id: "admin",
+    label: "Administrator",
+    descr: "An administrator"
+  }
+}
+
+export const mappings = {
+  [roles.guest.id]: ['stickers.find'],
+  [roles.agenda.id]: ['termin'],
+  [roles.mpa.id]: ['termin', 'article.find', 'auf.find', 'auf.create', 'auf.update',
+    'billable.find', 'billing.create',
+    'briefe', 'documents', 'fall', 'findings', 'konsultation', 'kontakt',
+    'labresults', 'leistungsblock', 'lucinda', 'macros', 'meta-article',
+    'patient', 'prescriptions', 'stickers', 'tarmed.find', 'tarmed.get'],
+  [roles.doc.id]: ['billing'],
+  [roles.user.id]: ['user.get']
+}
+
+// user role includes guest role and agenda rolw
+mappings[roles.user.id] = [...mappings[roles.guest.id], ...mappings[roles.user.id], ...mappings[roles.agenda.id]]
+// mpa role includes user role
+mappings[roles.mpa.id] = [...mappings[roles.user.id], ...mappings[roles.mpa.id]]
+// doctor role includes mpa role
+mappings[roles.doc.id] = [...mappings[roles.mpa.id], ...mappings[roles.doc.id]]
---
+++ server/src/util/acl.ts
@@ -0,0 +1,128 @@
+/***************************************
+ * This file is part of Webelexis(tm)
+ * Copyright (c) 2016-2020 by G. Weirich.
+ * All rights reserved.
+ ***************************************/
+
+/**
+ * Access control system:
+ * - Every resource is protected by a number of Access Control Elements (ACE). There are distinct ACEs for
+ * reading, writing, updating and deleting such a resource.
+ *
+ * - Every ACE has a parent ACE or ist the root Element.
+ *
+ * - Every user has one or more roles. At least they have the role `roles.guest`.
+ *
+ * - Every role has zero or more ACEs.
+ *
+ * - If a user wants to access a resource method, they must have the corresponding ACE (e.g. konktakt.find).
+ *  They have an ACE, if one of their roles has this ACE or a parent (or grandparent and so on) of this ACE.
+ *
+ * - A user who is member of the group `roles.admin` implicitely has all ACEs (i,e, has the ACE "root"
+ *  which is by convention ancestor of all other ACEs).
+ *
+ * Roles are installation specific and therefore are defined in config/roles.js
+ */
+
+const acls = new Map()
+import { roles as sysroles, mappings as mapper } from '../configuration'
+import logger from '../logger'
+
+
+class ACE {
+  private name
+  private parent
+  private fullname
+  constructor(name, parent) {
+    this.name = name
+    this.parent = parent
+    if (this.parent) {
+      this.fullname = this.parent.fullname + "." + this.name
+    } else {
+      this.fullname = name
+    }
+  }
+}
+
+const declareACE = (ace) => {
+  if (Array.isArray(ace)) {
+    for (const a of ace) {
+      declareACE(a)
+    }
+  } else {
+    acls.set(ace.name, ace)
+  }
+}
+
+/**
+ * Check if an action is allowed
+ * @param {*} usr The user in question
+ * @param {*} acename the action requested
+ */
+const hasRight = (usr, acename) => {
+  // if no usr is given, or they have no roles, it's a guest
+  if (!usr) {
+    usr = {}
+  }
+  if (!usr.roles) {
+    usr.roles = []
+  }
+  // if no ACE is requested, it's allowed
+  if (!acename) {
+    return true
+  }
+  // if no ace with the requested name exists, it's allowed
+  const ace = acls.get(acename)
+  if (!ace) {
+    return true
+  }
+
+  /**
+   * If the user is administrator, always allowed
+   */
+  if (usr.is_administrator == "1") {
+    return true
+  }
+  // id the user has the admin role, it's allowed
+  if (usr.roles.find(r => r == sysroles.admin.id)) {
+    return true
+  }
+  // Everyone has at least the guest role
+  if (!usr.roles.find(r => r == sysroles.guest.id)) {
+    usr.roles.push(sysroles.guest.id)
+  }
+  // Check if one of the roles of the user has the requested ACE
+  // or an antecestor of the requested ACE
+  for (const roleid of usr.roles) {
+    const acl = mapper[roleid]
+    // if the mapper contains acl for the given role, and the acl
+    // containts the requested right, ok
+    if (acl && acl.find(r => r == ace.name)) {
+      return true
+    }
+    // else if the ace has a parent, check that parent
+    if (ace.parent && hasRight(usr, ace.parent.name)) {
+      return true;
+    }
+  }
+  // if nothing matched, forbid it
+  return false
+}
+/**
+ * Check if the user has a right and throw an error if not.
+ * @param {*} usr
+ * @param {*} ace
+ */
+const needsRight = (usr, ace) => {
+  if (!hasRight(usr, ace)) {
+    logger.warn("%s has insufficient rights for %s", usr, ace)
+    throw new Error("insufficient rights")
+  }
+}
+
+module.exports = {
+  ACE,
+  declareACE,
+  hasRight,
+  needsRight
+}
