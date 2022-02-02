e411099970a6937cdfefcf4dbcbc9f3564499d06
gerry <weirich@elexis.ch>
2022-02-02 12:48:31 +0100

    details

--- client_v5/src/models/tree.spec.ts
+++ client_v5/src/models/tree.spec.ts
@@ -9,21 +9,21 @@ describe('Tree', () => {
     expect(stringComparator("bcde","abcd")).toBe(1)
     expect(stringComparator("abcd","abcd")).toBe(0)
     
-    const t1 = new Tree<string>(null, null)
+    const t1 = new Tree<string>(null, "parent")
     const t2 = new Tree<string>(t1, 'first child')
     const t3 = new Tree<string>(t1, 'second child')
     const t4 = new Tree<string>(t2, 'grandchild')
     const t5 = t1.insert('first child', stringComparator)
 
     expect(t1.getFirst().getPayload()).toBe(t3.getPayload())
-/*
-    expect(t2.getNext()).toBe(t3)
-    expect(t2.getParent()).toBe(t1)
-    expect(t3.getParent()).toBe(t1)
-    expect(t4.getParent()).toBe(t2)
+
+    expect(t3.getNext().getPayload()).toBe(t2.getPayload())
+    expect(t2.getNext()).toBeUndefined()
+    expect(t2.getParent().getPayload()).toBe(t1.getPayload())
+    expect(t3.getParent().getPayload()).toBe(t1.getPayload())
+    expect(t4.getParent().getPayload()).toBe(t2.getPayload())
     expect(t4.getNext()).toBeUndefined()
-    */
     expect((await t1.getChildren()).length).toBe(2)
-    
+    expect((await t2.getChildren()).length).toBe(1)
   })
 })
--- client_v5/src/services/billing.ts
+++ client_v5/src/services/billing.ts
@@ -1,15 +1,72 @@
 import { getService } from "./io";
 import type { EncounterType as Konsultation } from "../models/encounter-model";
-import { LazyTree } from "../models/tree";
+import { Tree, type ITreeListener } from "../models/tree";
+
+type konsdef = {
+  id: string, patientid: string, fallid: string,
+  Patient?: any
+  Fall?: any
+  Konsultation?: any
+}
+class PatientListener implements ITreeListener {
+  private patService
+  constructor(private all: Array<konsdef>) {
+    this.patService = getService("patient")
+  }
+  async fetchChildren(t: Tree<konsdef>): Promise<boolean> {
+    const konsdef = t.getPayload()
+    if (konsdef) {
+      konsdef.Patient = await this.patService.get(konsdef.patientid)
+      for (const cas of this.all) {
+        t.insert(cas, (a, b) => a.fallid.localeCompare(b.fallid), new FallListener(this.all))
+      }
+      return true
+    }
+    return false
+  }
+}
+class FallListener implements ITreeListener {
+  private fallService
+  constructor(private all: Array<konsdef>) {
+    this.fallService = getService("fall")
+  }
+  async fetchChildren(t: Tree<konsdef>): Promise<boolean> {
+    const konsdef = t.getPayload()
+    if (konsdef) {
+      konsdef.Fall = await this.fallService.get(konsdef.fallid)
+      for (const cas of this.all) {
+        t.insert(cas, (a, b) => a.fallid.localeCompare(b.fallid), new KonsListener(this.all))
+      }
+      return true
+    }
+    return false
+  }
+}
+class KonsListener implements ITreeListener {
+  private konsService
+  constructor(private all: Array<konsdef>) {
+    this.konsService = getService("konsultation")
+  }
+  async fetchChildren(t: Tree<konsdef>): Promise<boolean> {
+    const konsdef = t.getPayload()
+    if (konsdef) {
+      konsdef.Konsultation = await this.konsService.get(konsdef.id)
+      for (const kons of this.all) {
+        const p=new Tree<konsdef>(t,konsdef)
+      }
+      return true
+    }
+    return false
+  }
+}
 
 export class Billing {
-
-  async getBillables(): Promise<LazyTree<any> {
+  async getBillables(): Promise<Tree<any> {
     const konsService = getService("konsultation")
-    const unbilled:Array<{id:string,patientid:string,fallid:string}> = await konsService.get('unbilled')
-    const ret=new LazyTree<any>(null,null,null)
-    for(let p of unbilled){
-      
+    const unbilled: Array<konsdef> = await konsService.get('unbilled')
+    const ret = new Tree<konsdef>(null, null)
+    for (let p of unbilled) {
+      ret.insert(p, (a, b) => a.patientid.localeCompare(b.patientid), new PatientListener(unbilled))
     }
     return ret
   }
