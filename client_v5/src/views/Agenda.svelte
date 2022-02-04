5b9dc853b67761ced02c10f31145d1be88722486
rgwch <rgw@elexis.ch>
2022-02-04 07:19:36 +0100

    div

--- client_v5/src/views/Agenda.svelte
+++ client_v5/src/views/Agenda.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
   import Calendar from '../components/Calendar.svelte'
-  import {getService} from '../services/io'
-  import {DateTime} from 'luxon'
-  const agnService=getService('agntermine')
+  import { getService } from '../services/io'
+  import { DateTime } from 'luxon'
+  import Listview from '../components/Listview.svelte'
+  const agnService = getService('termin')
   let date: Date = new Date()
+  let list: Array<any> = []
   function select(event) {
-    const date=event.detail
-    agnService.find({query:{
-      tag: DateTime.fromJSDate(date).toFormat("yyyyLLdd"),
-      deleted: "0"
-    }})
+    const date = event.detail
+    agnService
+      .find({
+        query: {
+          tag: DateTime.fromJSDate(date).toFormat('yyyyLLdd'),
+          deleted: '0',
+        },
+      })
+      .then((result) => {
+        list = result.data
+      })
   }
 </script>
 
 <template>
-  <Calendar {date} on:{select} />
+  <div class="flex">
+    <Calendar {date} on:select={select} />
+    <Listview {list} />
+  </div>
 </template>
