44743a5e479800a60509a706cc20f07c59390bed
rgwch <weirich@elexis.ch>
2022-05-11 19:51:44 +0200

    tiptap

---
+++ client_v5/src/widgets/Editor.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+import { _ } from "svelte-i18n";
+import Editor from "./Editor.svelte";
+</script>
+
+<Meta title="Widgets/Editor" component="{Editor}" />
+
+<Template let:args>
+  <Editor {...args} />
+</Template>
+
+<Story name="Plain" />
