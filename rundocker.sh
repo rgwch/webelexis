25d073ec5954616e77725f06aa835819ce10a901
rgwch <weirich@elexis.ch>
2022-05-28 17:23:54 +0200

    docker ok

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker rm webelexis
-docker run -p 3031:3030 -p 4041:4040 --name webelexis -v `pwd`/data:/home/node/webelexis/data -v `pwd`/server/config:/home/node/webelexis/server/config rgwch/webelexis:`cat VERSION`
+docker run -p 3030:3030 -p 4040:4040 --name webelexis --network external_webelexis_net -v `pwd`/data:/home/node/webelexis/data -v `pwd`/server/config:/home/node/webelexis/server/config rgwch/webelexis:`cat VERSION`
