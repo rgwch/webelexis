84bb2c32792ee8bde003e500a7d3393e94c0f85c
rgwch <weirich@elexis.ch>
2022-06-03 12:50:22 +0200

    add apnt

--- client_v5/src/components/Newappointment.svelte
+++ client_v5/src/components/Newappointment.svelte
@@ -48,17 +48,17 @@ async function accept() {
     <div slot="body">
       <div class="panel">
         <h3>{$_("appointment.enternew")}</h3>
-        <div class="flex flex-columns">
+        <div class="flex flex-columns px-2">
           <span>{$_("appointment.time")}:</span>
-          <span>{timelabel}</span>
-          <span>{$_("appointment.duration")}</span>
-          <span><input type="number" bind:value="{duration}" /></span>
+          <span class="mx-2">{timelabel}</span>
           <Slider
             class="w-max inline-block"
             bind:value
             min="{termin.getBeginMinutes()}"
             max="{termin.getEndMinutes() - 5}"
             step="{5}" />
+          <span class="mx-2">{$_("appointment.duration")}</span>
+         <input class="w-10 mx-2 h-6" type="number" bind:value="{duration}" />
         </div>
         <p
           class="font-bold text-blue-700 cursor-pointer"
@@ -86,9 +86,11 @@ async function accept() {
               {/each}
             </select>
           </div>
-          <textarea class="w-max h-max" bind:value="{reason}"></textarea>
+          <textarea class="flex-1" bind:value="{reason}"></textarea>
         </div>
-        <button on:click="{accept}">{$_("appointment.enter")}</button>
+        <button type="button" on:click="{accept}" class="mt-5 inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{$_("appointment.enter")}</button>
+        <button type="button" on:click={()=>expanded=false} class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{$_("actions.cancel")}</button>
+
       </div>
     </div>
   </Collapse>
