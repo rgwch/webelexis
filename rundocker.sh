9d482ce54af221549bfd1b2e1542504db0a45424
gerry <weirich@elexis.ch>
2022-07-04 17:09:24 +0200

    details

--- rundocker.sh
+++ rundocker.sh
@@ -2,4 +2,4 @@
 
 docker stop webelexis
 docker rm webelexis
-docker run -p 3030:3030 -p 4040:4040 --name webelexis --network external_webelexis_net -v `pwd`/data:/home/node/webelexis/data -v `pwd`/server/config:/home/node/webelexis/server/config rgwch/webelexis:`cat VERSION`
+docker run -p 3030:3030 -p 4040:4040 --name webelexis --network external_webelexis_net -v `pwd`/data:/home/node/webelexis/data -v `pwd`/server/config:/home/node/webelexis/server/config -d rgwch/webelexis:`cat VERSION`
