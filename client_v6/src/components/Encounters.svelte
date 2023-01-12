4eb7d7c82d991977045eacf644fc0a1d4414c488
gerry <weirich@elexis.ch>
2023-01-12 14:54:48 +0100

    fix encounters endless scroll

--- client_v6/src/components/Encounters.svelte
+++ client_v6/src/components/Encounters.svelte
@@ -1,46 +1,54 @@
 <script lang="ts">
-import EncounterDetail from "./EncounterDetail.svelte";
-import { EncounterManager } from "../models/encounter-model";
-import type { EncounterType } from "../models/encounter-model";
-import type { PatientType } from "../models/patient-model";
-import InfiniteScroll from "svelte-infinite-scroll";
-import {currentPatient} from '../services/store'
-import { onMount } from "svelte";
-const BATCHSIZE=5
-const em = new EncounterManager();
-let offset = 0;
-let newBatch = [];
-let encounters: Array<EncounterType> = [];
+  import EncounterDetail from "./EncounterDetail.svelte";
+  import { EncounterManager } from "../models/encounter-model";
+  import type { EncounterType } from "../models/encounter-model";
+  import type { PatientType } from "../models/patient-model";
+  import InfiniteScroll from "svelte-infinite-scroll";
+  import { currentPatient } from "../services/store";
+  import { onMount } from "svelte";
+  const BATCHSIZE = 11;
+  const em = new EncounterManager();
+  let offset = 0;
+  let newBatch = [];
+  let encounters: Array<EncounterType> = [];
 
-  currentPatient.subscribe(np=>{
+  currentPatient.subscribe((np) => {
     offset = 0;
     newBatch = [];
     encounters = [];
     fetchEncounters();
-  })
+  });
 
-async function fetchEncounters() {
-  const result = await em.fetchForPatient($currentPatient?.id, offset, BATCHSIZE);
-  newBatch = result.data.map((e) => {(e._Patient = $currentPatient); return e});
-  offset = result.skip + result.data.length;
-}
+  async function fetchEncounters() {
+    const result = await em.fetchForPatient(
+      $currentPatient?.id,
+      offset,
+      BATCHSIZE
+    );
+    newBatch = result.data.map((e) => {
+      e._Patient = $currentPatient;
+      return e;
+    });
+    offset = result.skip + result.data.length;
+  }
 
-$: encounters = [...encounters, ...newBatch];
+  $: encounters = [...encounters, ...newBatch];
 
-onMount(() => {
-  offset = 0;
-  fetchEncounters();
-});
+  onMount(() => {
+    offset = 0;
+    fetchEncounters();
+  });
 </script>
 
 <template>
   <div class="scrollpanel">
     {#each encounters as encounter}
-      <EncounterDetail entity="{encounter}" />
+      <EncounterDetail entity={encounter} />
     {/each}
     <InfiniteScroll
-      hasMore="{newBatch.length > 0}"
-      threshold="{50}"
-      on:loadMore="{() => fetchEncounters()}" />
+      hasMore={newBatch.length > 0}
+      threshold={10}
+      on:loadMore={() => fetchEncounters()}
+    />
   </div>
 </template>
