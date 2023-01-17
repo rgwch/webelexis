21504e0eb96171833afbf2de376e8b0e0028a1d6
gerry <weirich@elexis.ch>
2023-01-17 13:34:50 +0100

    patient selector

--- client_v6/src/components/PatientSelector.svelte
+++ client_v6/src/components/PatientSelector.svelte
@@ -1,42 +1,46 @@
 <script lang="ts">
-import { PatientManager, Patient } from "../models/patient-model";
-import type { PatientType } from "../models/patient-model";
-import Fa from "svelte-fa";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { _ } from "svelte-i18n";
-import { createEventDispatcher } from "svelte";
-import {currentPatient}  from "../services/store";
-import LineInput from "../widgets/LineInput.svelte";
-const dispatch = createEventDispatcher();
+  import { PatientManager, Patient } from "../models/patient-model";
+  import type { PatientType } from "../models/patient-model";
+  import Fa from "svelte-fa";
+  import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+  import { _ } from "svelte-i18n";
+  import { createEventDispatcher } from "svelte";
+  import { currentPatient } from "../services/store";
+  import LineInput from "../widgets/LineInput.svelte";
+  const dispatch = createEventDispatcher();
 
-const pm = new PatientManager();
-let found: Array<PatientType> = [];
-let searchTerm = "";
-async function doSearch() {
-  const result = await pm.find({ query: { $find: searchTerm } });
-  found = result.data;
-}
-function select(entry) {
-  currentPatient.set(entry);
-  dispatch("selected", entry);
-}
+  const pm = new PatientManager();
+  let found: Array<PatientType> = [];
+  let searchTerm = "";
+  async function doSearch() {
+    const result = await pm.find({ query: { $find: searchTerm } });
+    found = result.data;
+  }
+  function select(entry) {
+    currentPatient.set(entry);
+    dispatch("selected", entry);
+  }
 </script>
 
 <template>
   <div>
     <LineInput
-      label="{$_('prompts.patsearch')}"
-      bind:value="{searchTerm}"
-      on:textChanged="{doSearch}"
-      buttonIcon="{faMagnifyingGlass}" />
+      label={$_("prompts.patsearch")}
+      bind:value={searchTerm}
+      on:textChanged={doSearch}
+      buttonIcon={faMagnifyingGlass}
+    />
   </div>
-  <div class="border-1 overflow-auto h-20">
-    {#each found as entry}
-      <p
-        class="my-0 cursor-pointer hover:text-blue-400"
-        on:click="{() => select(entry)}">
-        {@html Patient.getLabel(entry)}
-      </p>
-    {/each}
-  </div>
+  {#if found.length > 0}
+    <div class="border-1 overflow-auto h-80">
+      {#each found as entry}
+        <p
+          class="my-0 cursor-pointer hover:text-blue-400"
+          on:click={() => select(entry)}
+        >
+          {@html Patient.getLabel(entry)}
+        </p>
+      {/each}
+    </div>
+  {/if}
 </template>
