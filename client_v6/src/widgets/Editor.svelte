bc8c46429d4eefded047cd7a2bfacc03ee4a5b2a
rgwch <weirich@elexis.ch>
2023-01-20 07:16:15 +0100

    editor

--- client_v6/src/widgets/Editor.svelte
+++ client_v6/src/widgets/Editor.svelte
@@ -1,78 +1,83 @@
 <script lang="ts">
-import { onMount, onDestroy, createEventDispatcher } from "svelte";
-import { Editor } from "@tiptap/core";
-import StarterKit from "@tiptap/starter-kit";
-const dispatch = createEventDispatcher();
+  import { onMount, onDestroy, createEventDispatcher } from "svelte";
+  import { Editor } from "@tiptap/core";
+  import StarterKit from "@tiptap/starter-kit";
+  const dispatch = createEventDispatcher();
 
-export let contents;
-export let extensions = [];
-export let editable = true;
+  export let contents;
+  export let extensions = [];
+  export let editable = true;
 
-let element;
-let editor;
+  let element;
+  let editor;
 
-/*
+  /*
 $: {
   console.log("Changed "+contents)
   editor?.commands.setContent(contents)
 }
 */
 
-onMount(() => {
-  editor = new Editor({
-    editorProps: {
-      attributes: {
-        class: "bg-blue-200",
+  onMount(() => {
+    editor = new Editor({
+      editorProps: {
+        attributes: {
+          class: "bg-blue-100 px-2",
+        },
       },
-    },
-    element: element,
-    extensions: [StarterKit, ...extensions],
-    content: contents,
-    editable,
-    
-    onBlur: () => {
-      dispatch("changed", editor.getJSON());
-    },
-    onTransaction: (event) => {
-      // force re-render so `editor.isActive` works as expected
-      editor = editor;
-    },
-  });
-});
+      element: element,
+      extensions: [StarterKit, ...extensions],
+      content: contents,
+      editable,
 
-onDestroy(() => {
-  if (editor) {
-    editor.destroy();
-  }
-});
+      onBlur: () => {
+        dispatch("changed", editor.getJSON());
+      },
+      onTransaction: (event) => {
+        // force re-render so `editor.isActive` works as expected
+        editor = editor;
+      },
+      onUpdate: (props) => {
+       
+        // editor.command(tr=>{tr.insertText("test")})
+      },
+    });
+  });
+
+  onDestroy(() => {
+    if (editor) {
+      editor.destroy();
+    }
+  });
 </script>
 
 <template>
-  {#if editor}
+  {#if false}
     <button
-      on:click="{() =>
-        editor.chain().focus().toggleHeading({ level: 1 }).run()}"
-      class:active="{editor.isActive('heading', { level: 1 })}">
+      on:click={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+      class:active={editor.isActive("heading", { level: 1 })}
+    >
       H1
     </button>
     <button
-      on:click="{() =>
-        editor.chain().focus().toggleHeading({ level: 2 }).run()}"
-      class:active="{editor.isActive('heading', { level: 2 })}">
+      on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+      class:active={editor.isActive("heading", { level: 2 })}
+    >
       H2
     </button>
     <button
-      on:click="{() => editor.chain().focus().setParagraph().run()}"
-      class:active="{editor.isActive('paragraph')}">
+      on:click={() => editor.chain().focus().setParagraph().run()}
+      class:active={editor.isActive("paragraph")}
+    >
       P
     </button>
   {/if}
-  <div bind:this="{element}" class="border-2"></div>
+  <div bind:this={element} class="border-2" />
 
   <style>
-  button.active {
-    background: black;
-    color: white;
-  }
+    button.active {
+      background: black;
+      color: white;
+    }
   </style>
 </template>
