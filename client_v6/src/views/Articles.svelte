aea3ba648cad43f9378734e9779b83dbe1329a4c
rgwch <weirich@elexis.ch>
2023-06-22 09:15:20 +0200

    article concept

--- client_v6/src/views/Articles.svelte
+++ client_v6/src/views/Articles.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
   import Barcode from "../components/Barcode.svelte"
+  import {prescriptionManager} from '../models'
   const title = +"rticle";
+  async function findByEAN(ean:string){
+    const result=await prescriptionManager.findArticle("ean="+ean)
+    return result;
+  }
 </script>
 <template>
   <div class="flex">
