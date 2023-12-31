629932e6fde3b1c9733259ea62c6bb0280b3807a
rgwch <weirich@elexis.ch>
2023-12-31 08:15:36 +0100

    encounters stats

--- client_v6/src/components/EncountersByDate.svelte
+++ client_v6/src/components/EncountersByDate.svelte
@@ -8,12 +8,15 @@
   import Collapse from "../widgets/Collapse.svelte";
   import DateInput from "../widgets/DateInput.svelte";
   import { Money } from "../models/money";
+  import { each } from "svelte/internal";
   export let fromDate: string = DateTime.now().toFormat("yyyyLLdd");
   export let untilDate: string = DateTime.now().toFormat("yyyyLLdd");
   let list: Array<EncounterModel> = [];
   let patients = new Set<string>();
   let konsen = 0;
   let total: Money = new Money(0);
+  let byLaw = new Map<string, { encounters: number; sum: Money }>();
+  let lawnames=[]
   let searching = false;
 
   async function reload() {
@@ -21,17 +24,32 @@
     patients.clear();
     konsen = 0;
     total = new Money(0);
+    byLaw = new Map<string, { encounters: number; sum: Money }>();
+  
     const raw: Array<EncounterModel> = (
       await encounterManager.fetchForTimes(fromDate, untilDate, "")
     ).map((k) => new EncounterModel(k));
     for (const k of raw) {
       await k.getPatient();
-      total = total.add(await k.getSum());
+      const fall = await k.getCase();
+      const law = fall.gesetz;
+      const sum = await k.getSum();
+      total = total.add(sum);
       patients = patients.add(k.entity._Patient.id);
       konsen += 1;
+      let entry;
+      if (byLaw.has(law)) {
+        entry = byLaw.get(law);
+        entry.encounters += 1;
+        entry.sum = entry.sum.add(sum);
+      } else {
+        entry = { encounters: 1, sum };
+      }
+      byLaw.set(law, entry);
     }
     searching = false;
     list = raw;
+    lawnames=[...byLaw.keys()]
   }
   // bg-blue-500 font-bolder text-white px-3 py-2 mx-4 mt-4 transition duration-300 ease-in-out hover:bg-blue-600 rounded-full
 </script>
@@ -49,6 +67,9 @@
     <span
       >{patients.size} Patienten, {konsen} Konsultationen, Total CHF {total.getFormatted()}</span
     >
+    {#each lawnames as lawname}
+      <p class="ml-4">{lawname}: {byLaw.get(lawname).encounters} Konsultationen, Total CHF {byLaw.get(lawname).sum.getFormatted()}</p>
+    {/each}
   </div>
   {#if searching}
     <img src="webelexis-anim.gif" width="150px" alt="wait..." />
